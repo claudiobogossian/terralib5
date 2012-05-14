08e796f67d1efe0bf19b44e6d3f89d2d39d2a19a
Gilberto <gribeiro@dpi.inpe.br>
2012-05-14 09:23:03 -0300

    adding new macros adding new concepts fixing small doc style

---
+++ src/terralib/common/SystemApplicationSettings.h
@@ -0,0 +1,85 @@
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/common/SystemApplicationSettings.h
+
+  \brief A singleton for managing application settings applied to the whole system (all users).
+*/
+
+#ifndef __TERRALIB_COMMON_INTERNAL_SYSTEMAPPLICATIONSETTINGS_H
+#define __TERRALIB_COMMON_INTERNAL_SYSTEMAPPLICATIONSETTINGS_H
+
+// TerraLib
+#include "ApplicationSettings.h"
+#include "Singleton.h"
+
+namespace te
+{
+  namespace common
+  {
+    /*!
+      \class SystemApplicationSettings
+
+      \brief A singleton for managing application settings applied to the whole system (all users).
+
+      This singleton can be used to keep the application configuration.
+
+      It will look for an application configuration file as follows:
+      <ul>
+      <li>first, searchs for the application configuration file in the current application dir</li>
+      <li>then it check for this file in the application data dir (plataform dependent!)</li>
+      <li>use a location defined by an environment variable TERRALIB_DIR_ENVVAR and a sub-dir defined by the macro TERRALIB_CONFIG_DIR</li>
+      </ul>
+    */
+    class TECOMMONEXPORT SystemApplicationSettings
+      : public ApplicationSettings<ObjectLevelLockable,
+                                   ::boost::recursive_mutex,
+                                   ::boost::lock_guard<::boost::recursive_mutex>,
+                                   ::boost::lock_guard<::boost::recursive_mutex> >,
+        public Singleton<SystemApplicationSettings>
+    {
+      friend class Singleton<SystemApplicationSettings>;
+
+      public:
+
+        /*! \brief It tries to find a default config file based on system macros and default condigurations. */
+        void load();
+
+        /*!
+          \brief It loads the setting from the given file.
+          
+          \param fileName It must be the full path to the settings file.
+        */
+        void load(const std::string& fileName);
+
+      protected:
+
+        /*! \brief It initializes the singleton. */
+        SystemApplicationSettings();
+
+        /*! \brief Destructor. */
+        ~SystemApplicationSettings();
+    };
+
+  } // end namespace common
+}   // end namespace te
+
+#endif  // __TERRALIB_COMMON_INTERNAL_SYSTEMAPPLICATIONSETTINGS_H
+
---
+++ src/terralib/common/UserApplicationSettings.cpp
@@ -0,0 +1,109 @@
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/common/UserApplicationSettings.cpp
+
+  \brief A singleton for managing application settings applied to a single user.
+*/
+
+// TerraLib
+#include "Exception.h"
+#include "OSSettingsDir.h"
+#include "SystemApplicationSettings.h"
+#include "Translator.h"
+#include "UserApplicationSettings.h"
+
+// STL
+#include <cstdlib>
+
+// Boost
+#include <boost/filesystem.hpp>
+
+void te::common::UserApplicationSettings::load()
+{
+  std::string user_conf_file_name = SystemApplicationSettings::getInstance().getValue("Application.UserSettingsFile.<xmlattr>.xlink:href");
+
+  if(user_conf_file_name.empty())
+    user_conf_file_name = TERRALIB_USER_SETTINGS_FILE;
+
+// first: current application dir
+  boost::filesystem::path user_settings_file = boost::filesystem::current_path();
+  
+  user_settings_file /= TERRALIB_CONFIG_DIR;
+
+  user_settings_file /= user_conf_file_name;
+
+  if(boost::filesystem::is_regular_file(user_settings_file))
+  {
+    load(user_settings_file.string());
+    return;
+  }
+
+// second: user data dir
+  const std::string& user_data_dir = OSSettingsDir::getInstance().getUserSettingsPath();
+
+  if(!user_data_dir.empty())
+  {
+    user_settings_file = user_data_dir;
+
+    user_settings_file /= TERRALIB_DIR;
+
+    user_settings_file /= user_conf_file_name;
+
+    if(boost::filesystem::is_regular_file(user_settings_file))
+    {
+      load(user_settings_file.string());
+      return;
+    }
+  }
+
+// the last chance...
+  char* mgis_dir = getenv(TERRALIB_DIR_ENVVAR);
+
+  if(mgis_dir != 0)
+  {
+    user_settings_file = mgis_dir;
+
+    user_settings_file /= TERRALIB_CONFIG_DIR;
+
+    user_settings_file /= user_conf_file_name;
+
+    if(boost::filesystem::is_regular_file(user_settings_file))
+    {
+      load(user_settings_file.string());
+      return;
+    }
+  }
+}
+
+void te::common::UserApplicationSettings::load(const std::string& fileName)
+{
+  ApplicationSettings::load(fileName);
+}
+
+te::common::UserApplicationSettings::UserApplicationSettings()
+{
+}
+
+te::common::UserApplicationSettings::~UserApplicationSettings()
+{
+}
+
+
