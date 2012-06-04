94e64724d7c0b73ad6bc247f4e46d080e691f663
Lubia Vinhas <lubia@dhcp104.dpi.inpe.br>
2012-06-04 15:56:28 -0300

    closes #76 : the starting point to design automatic tests for module SRS (class instantiation and coordinate conversion) can be found in the examples/srs.

--- unittest/srs/TsSrs.cpp
+++ unittest/srs/TsSrs.cpp
@@ -12,21 +12,21 @@ CPPUNIT_TEST_SUITE_REGISTRATION( TsSrs );
 
 void TsSrs::tcEPSGFactory()
 {
-  te::srs::CoordinateSystem* cs = te::srs::CoordinateSystemFactory::make("EPSG", 4618);
+  te::srs::CoordinateSystem* cs = te::srs::CoordinateSystemFactory::make("EPSG", TE_SRS_SAD69);
   CPPUNIT_ASSERT_MESSAGE("Couldn't obtain a valid Coordinate System",cs);
   std::string name = cs->getName();
   CPPUNIT_ASSERT_MESSAGE("Couldn't obtain the right coordinate system",(name == "SAD 69"));
   std::string wkt = cs->getWKT();
   delete cs;
 
-  cs = te::srs::CoordinateSystemFactory::make("EPSG", 29168);
+  cs = te::srs::CoordinateSystemFactory::make("EPSG", TE_SRS_SAD69_UTM_ZONE_18N);
   CPPUNIT_ASSERT_MESSAGE("Couldn't obtain a valid Coordinate System",cs);
   name = cs->getName();
   CPPUNIT_ASSERT_MESSAGE("Couldn't obtain the right coordinate system",(name == "SAD 69 / UTM zone 18N"));
   wkt = cs->getWKT();
   delete cs;
 
-  cs = te::srs::CoordinateSystemFactory::make("EPSG", 32717);
+  cs = te::srs::CoordinateSystemFactory::make("EPSG", TE_SRS_WGS84_UTM_ZONE_17S);
   CPPUNIT_ASSERT_MESSAGE("Couldn't obtain a valid Coordinate System",cs);
   name = cs->getName();
   CPPUNIT_ASSERT_MESSAGE("Couldn't obtain the right coordinate system",(name == "WGS 84 / UTM zone 17S"));
@@ -37,34 +37,6 @@ void TsSrs::tcEPSGFactory()
 
 void TsSrs::setUp()
 {
-  te::common::UnitOfMeasure* m = new te::common::UnitOfMeasure(0,te::common::UnitOfMeasure::LengthMeasure);
-  m->setName("METER");
-  m->setSymbol("m");
-  std::vector<std::string> mNames;
-  mNames.push_back("METRE");  
-  te::common::UnitsOfMeasureManager::getInstance().insert(m,mNames);  
-
-  te::common::UnitOfMeasure* km = new te::common::UnitOfMeasure(1,te::common::UnitOfMeasure::LengthMeasure);
-  km->setName("KILOMETER");
-  km->setSymbol("km");
-  std::vector<std::string> kmNames;
-  kmNames.push_back("KILOMETRE");
-  km->setBaseUnitId(0);
-  km->setConversionFactors(0.001);
-  te::common::UnitsOfMeasureManager::getInstance().insert(km,kmNames);  
-
-  te::common::UnitOfMeasure* rad = new te::common::UnitOfMeasure(2,te::common::UnitOfMeasure::AngleMeasure);
-  rad->setName("RADIAN");
-  rad->setSymbol("rad");
-  te::common::UnitsOfMeasureManager::getInstance().insert(rad);  
-
-  te::common::UnitOfMeasure* deg = new te::common::UnitOfMeasure(3,te::common::UnitOfMeasure::AngleMeasure);
-  deg->setName("DEGREE");
-  deg->setSymbol("deg");
-  deg->setBaseUnitId(2);
-  deg->setConversionFactors(.0174532925199432958); 
-  te::common::UnitsOfMeasureManager::getInstance().insert(deg); 
-  
 }
 
 
