508e6344515ffb14927e0ebbb2fc6976da914fec
Frederico Bedê <frederico.bede@funcate.org.br>
2012-06-08 15:40:22 -0300

    testes unitarios funcionando.

--- build/Modules/FindCppUnit.cmake
+++ build/Modules/FindCppUnit.cmake
@@ -9,29 +9,30 @@
 # CPPUNIT_LIBRARY_RELEASE - The CppUnit release library
 
 find_path(CPPUNIT_INCLUDE_DIR cppunit/Test.h
-          PATHS "$ENV{TERRALIB_DEP_MSVC2010}/cppunit/include"
-                /usr/local/include/cppunit /usr/include/cppunit)
+          PATHS /usr/local/include/cppunit /usr/include/cppunit)
                 
-if(WIN32)
+#if(WIN32)
   find_library(CPPUNIT_LIBRARY_DEBUG
-               NAMES cppunitd_dll
-               PATHS "$ENV{TERRALIB_DEP_MSVC2010}/cppunit/src/cppunit/x86/DebugDll")
+               NAMES cppunitd_dll cppunit
+               PATHS /usr/local/lib /lib /usr/lib
+               )
                               
   find_library(CPPUNIT_LIBRARY_RELEASE
-               NAME cppunit_dll
-               PATHS "$ENV{TERRALIB_DEP_MSVC2010}/cppunit/src/cppunit/x86/ReleaseDll")
+               NAME cppunit_dll cppunit
+               PATHS /usr/local/lib /lib /usr/lib
+               )
                
                
   checkLibNames("CPPUNIT")
   
-else(WIN32)
-  find_library(CPPUNIT_LIBRARY
-               NAME cppunit
-               PATHS /usr/local/lib /lib /usr/lib)
-endif(WIN32)                
+#else(WIN32)
+ # find_library(CPPUNIT_LIBRARY
+  #             NAME cppunit
+   #            PATHS /usr/local/lib /lib /usr/lib)
+#endif(WIN32)                
 
 set (CPPUNIT_FOUND FALSE)
- 
+
 if(CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
   set(CPPUNIT_FOUND TRUE)
   message(STATUS "-- Found CppUnit.")
