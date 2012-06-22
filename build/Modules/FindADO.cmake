3c10fe35de301a4904eff78c5467135e8d01e96a
Felipe Siqueira <felipe.siqueira@funcate.org.br>
2012-06-22 13:17:24 -0300

    FindADO from 32 bits only OS

--- build/Modules/FindADO.cmake
+++ build/Modules/FindADO.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 2.8)
 
 if( CMAKE_SIZEOF_VOID_P EQUAL 4 )
-  if("$ENV{CommonProgramFiles(x86)}" EQUAL "")
+  if(NOT "$ENV{CommonProgramFiles(x86)}")
     set (path_prefix "$ENV{CommonProgramFiles}")
   else()
     set (path_prefix "$ENV{CommonProgramFiles(x86)}")
