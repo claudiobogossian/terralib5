6b0ead44d3d417a90735fc0eee7c4abe23740a08
marisa <marisa@dpi.inpe.br>
2012-08-30 15:30:57 -0300

    Removing commented lines

--- src/terralib/common/BoostUtils.cpp
+++ src/terralib/common/BoostUtils.cpp
@@ -76,49 +76,6 @@ void te::common::Convert(const boost::property_tree::ptree& p, std::vector<size_
   }
 }
 
-
-////void te::common::Convert(std::vector<double>vectd, te::gm::Envelope& env )
-////{
-////   std::vector<double>::iterator it;
-////   double  d[4]={0.0,0.0,0.0,0.0}; int i=0;
-////   for(it = vectd.begin(); it < vectd.end(); it++)
-////   {
-////       d[i] = (*it);       
-////       i+= 1;
-////   }
-////   std::string s;
-////   s = "marisa";
-////   double  d0 = boost::lexical_cast<const double >(d[0]);
-////   double  d1 = boost::lexical_cast<const double >(d[1]);
-////   double  d2 = boost::lexical_cast<const double >(d[2]);
-////   double  d3 = boost::lexical_cast<const double >(d[3]);
-////   
-////   te::gm::Envelope const env_aux(d0,d1,d2,d3);
-////   //env.operator=(env_aux);
-////   env.init(d[0],d[1],d[2],d[3]);
-////   //env.init(d0,d1,d2,d3);
-////   std::string s1;
-////   s1 = "marisa1";
-////}
-
-////void te::common::Convert(const boost::property_tree::ptree& p, std::vector<te::gm::Envelope>& vect)
-////{
-////  BOOST_FOREACH(const boost::property_tree::ptree::value_type& v, p)
-////  {
-////    BOOST_FOREACH(const boost::property_tree::ptree::value_type& v2, v.second.get_child(""))
-////    {
-////      te::gm::Envelope env_i;
-////      std::vector<double> envrep;
-////      te::common::Convert(v2.second.get_child(""), envrep); 
-////      te::common::Convert(envrep, env_i);
-////      vect.push_back(env_i);
-////      int xx;
-////      xx=0;
-////
-////    }
-////  }
-////}
-
 void te::common::Convert(const boost::property_tree::ptree& p, std::vector<std::vector<double>>& vect)
 {
   BOOST_FOREACH(const boost::property_tree::ptree::value_type& v, p)
@@ -127,22 +84,15 @@ void te::common::Convert(const boost::property_tree::ptree& p, std::vector<std::
     {
       std::vector<double> envrep;
       te::common::Convert(v2.second.get_child(""), envrep);
-      //te::gm::Envelope envelope_res;
-      //te::common::Convert(envrep, envelope_res);
-      {
-          std::vector<double>::iterator it;
-          double  d[4]={0.0,0.0,0.0,0.0}; int i=0;
-          for(it = envrep.begin(); it < envrep.end(); it++)
-           {
-               d[i] = (*it);       
-               i+= 1;
-           }
-          //te::gm::Envelope envelope1;
-          //(d[0],d[1],d[2],d[3]);
-          //double dd1 = envelope1.getLowerLeftX();
-           std::string s;
-           s = "marisa";
-      }
+      //{
+      //    std::vector<double>::iterator it;
+      //    double  d[4]={0.0,0.0,0.0,0.0}; int i=0;
+      //    for(it = envrep.begin(); it < envrep.end(); it++)
+      //     {
+      //         d[i] = (*it);       
+      //         i+= 1;
+      //     }
+      //}
       vect.push_back(envrep);
     }
   }
