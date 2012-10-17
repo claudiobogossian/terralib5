44c636d01c356da3e28bdaba6633b5dfc4902f93
Lubia Vinhas <lubia@dhcp88.dpi.inpe.br>
2012-10-17 17:40:08 -0300

    refs #208 Initial examples to test the components.

---
+++ examples/qt/datasource/main.cpp
@@ -0,0 +1,64 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file main.cpp
+
+  \brief A list of examples for the TerraLib Widgets.
+ */
+
+#include "MainWindow.h"
+
+#include <terralib/common.h>
+
+#include <QtGui/QApplication>
+#include <QtGui/qmessagebox.h>
+
+int main(int argc, char** argv)
+{
+  TerraLib::getInstance().initialize();  
+  
+  try
+  {
+    QApplication app(argc, argv);
+    
+    MainWindow window;
+    window.show();
+    
+    app.exec();
+    
+  }
+  catch(const std::exception& e)
+  {
+    std::cout << std::endl << "An unexpected exception has occuried!" << std::endl;
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();
+    
+    return EXIT_FAILURE;
+  }
+  
+  // finalize Terralib support
+  TerraLib::getInstance().finalize();
+  
+  std::cout << "Press Enter to exit..." << std::endl;
+  std::cin.get();
+  
+  return EXIT_SUCCESS;
+
+}
