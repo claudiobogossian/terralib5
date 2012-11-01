c9b29d75ddaf94bd4d4a1236aa66fe4bef1cff9a
Douglas Uba <douglas@dpi.inpe.br>
2012-11-01 09:41:17 -0200

    Adding a MapDisplay extension with multi-threading support;

---
+++ src/terralib/qt/widgets/canvas/DrawLayerThread.cpp
@@ -0,0 +1,104 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file DrawLayerThread.cpp
+
+  \brief This class represents a thread responsible to draw a given layer. Basically, this class receives draw layer requests and generates a QImage as result.
+*/
+
+// TerraLib
+#include "../../../geometry/Envelope.h"
+#include "../../../maptools/AbstractLayer.h"
+#include "DrawLayerThread.h"
+#include "Canvas.h"
+
+te::qt::widgets::DrawLayerThread::DrawLayerThread(QWidget* parent)
+  : QThread(parent),
+    m_abort(false)
+{
+  // Signals & slots
+  connect(&m_feedback, SIGNAL(timeout()), SLOT(sendFeedback()));
+  connect(this, SIGNAL(finished()), SLOT(stopSendFeedback())); 
+}
+
+te::qt::widgets::DrawLayerThread::~DrawLayerThread()
+{
+  m_mutex.lock();
+
+  m_abort = true;
+
+  m_mutex.unlock();
+
+  wait();
+}
+
+void te::qt::widgets::DrawLayerThread::draw(te::map::AbstractLayer* layer, const te::gm::Envelope& box, int srid, const QSize& size, const int& index)
+{
+  /* Note: For while... Actually, I would like to can stop the current draw process and restart with the new request box! te::common::TaskProgress?! */
+  if(isRunning())
+    return;
+
+  m_abort = false;
+
+  // Storing the values
+  m_layer = layer;
+  m_env = box;
+  m_srid = srid;
+  m_index = index;
+
+  // Creates the result image
+  if(m_image.size() != size)
+  {
+    m_image = QImage(size, QImage::Format_ARGB32_Premultiplied);
+    m_image.fill(qRgba(0, 0, 0, 0));
+  }
+
+  // Starts the timer feedback
+  m_feedback.start(100);
+
+  // Requests the thread execution!
+  start();
+}
+
+void te::qt::widgets::DrawLayerThread::run()
+{
+  // Prepares the canvas
+  Canvas canvas(&m_image);
+  canvas.calcAspectRatio(m_env.m_llx, m_env.m_lly, m_env.m_urx, m_env.m_ury);
+  canvas.setWindow(m_env.m_llx, m_env.m_lly, m_env.m_urx, m_env.m_ury);
+  canvas.clear();
+
+  // Let's draw!
+  m_layer->draw(&canvas, m_env, m_srid);
+
+  // Draw finished!
+  emit drawLayerFinished(m_index, m_image);
+  m_image.fill(qRgba(0, 0, 0, 0));
+}
+
+void te::qt::widgets::DrawLayerThread::sendFeedback()
+{
+  emit feedback(m_image);
+}
+
+void te::qt::widgets::DrawLayerThread::stopSendFeedback()
+{
+  m_feedback.stop();
+}
---
+++ src/terralib/qt/widgets/canvas/MultiThreadMapDisplay.h
@@ -0,0 +1,157 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file MultiThreadMapDisplay.h
+
+  \brief A multi thread Qt4 widget to control the display of a set of layers.
+*/
+
+#ifndef __TERRALIB_QT_WIDGETS_INTERNAL_MULTITHREAD_MAPDISPLAY_H
+#define __TERRALIB_QT_WIDGETS_INTERNAL_MULTITHREAD_MAPDISPLAY_H
+
+// TerraLib
+#include "MapDisplay.h"
+
+// Qt
+#include <QtGui/QImage>
+
+// STL
+#include <vector>
+
+namespace te
+{
+  namespace qt
+  {
+    namespace widgets
+    {
+// Forward declarations
+      class DrawLayerThread;
+
+      /*!
+        \class MultiThreadMapDisplay
+
+        \brief A multi thread Qt4 widget to control the display of a set of layers.
+      */
+      class TEQTWIDGETSEXPORT MultiThreadMapDisplay : public MapDisplay
+      {
+        Q_OBJECT
+
+        public:
+
+          /** @name Initializer Methods
+           *  Methods related to instantiation and destruction.
+           */
+          //@{
+
+          /*!
+            \brief It constructs an empty multi thread map display with the given dimensions which is a child of parent, with widget flags set to f.
+
+            \param size The map display size.
+            \param showFeedback A flag that indicates if the map display will show drawing feedback.
+            \param parent The widget's parent.
+            \param f Widget window flags.
+          */
+          MultiThreadMapDisplay(const QSize& size, const bool& showFeedback = true, QWidget* parent = 0, Qt::WindowFlags f = 0);
+
+          /*! \brief Destructor. */
+          ~MultiThreadMapDisplay();
+
+          //@}
+
+          /** @name MapDisplay Methods
+           *  Methods to configure the MapDisplay.
+           */
+          //@{
+
+          virtual void setLayerList(const std::list<te::map::AbstractLayer*>& order);
+
+          virtual void setExtent(const te::gm::Envelope& e);
+
+          //@}
+
+          /*!
+            \brief Transforms the given point, in screen coordinates, to a point in world coordinates.
+
+            \param p A point in screen coordinates.
+
+            \return The point in world coordinates.
+
+            \note This method will return a null point if the transform can not be done.
+          */
+          virtual QPointF transform(const QPointF& p);
+
+        protected:
+
+          /*!
+            \brief It displays the all layers.
+
+            \note It may need to retrieve data in order top display the layers.
+          */
+          virtual void draw();
+
+        private:
+
+          void updateTransform(); /*  Note: For while... I need the class CoordTransform! */
+
+        protected slots:
+
+          void showFeedback(const QImage& image);
+
+          void onDrawLayerFinished(const int& index, const QImage& image);
+
+        private:
+
+          /** @name Copy Constructor and Assignment Operator
+           *  Copy constructor and assignment operator not allowed.
+           */
+          //@{
+
+          /*!
+            \brief Copy constructor not allowed.
+
+            \param rhs The right-hand-side copy that would be used to copy from.
+          */
+          MultiThreadMapDisplay(const MultiThreadMapDisplay& rhs);
+
+          /*!
+            \brief Assignment operator not allowed.
+
+            \param rhs The right-hand-side copy that would be used to copy from.
+
+            \return A reference to this object.
+          */
+          MultiThreadMapDisplay& operator=(const MultiThreadMapDisplay& rhs);
+
+          //@}
+
+        protected:
+
+          std::vector<DrawLayerThread*> m_threads; // The set of threads used to draw the layer list.
+          std::map<int, QImage> m_images;          // The set of images built by each thread. It will be used to compose the final result, keeping the layer list order.
+          bool m_showFeedback;                     // A flag that indicates if the map display will show drawing feedback.
+
+          QMatrix m_matrix; /*  Used to converts coordinates screen -> world. Note: For while... I need the class CoordTransform! */
+      };
+
+    } // end namespace widgets
+  }   // end namespace qt
+}     // end namespace te
+
+#endif  // __TERRALIB_QT_WIDGETS_INTERNAL_MULTITHREAD_MAPDISPLAY_H
