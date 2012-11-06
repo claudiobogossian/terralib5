59bd61b584af3a828d1a1bfa525afa0a77631144
Felipe Siqueira <felipe.siqueira@funcate.org.br>
2012-11-06 08:52:21 -0200

    ADO Driver improvements

--- src/terralib/ado/DataSourceCatalogLoader.h
+++ src/terralib/ado/DataSourceCatalogLoader.h
@@ -32,11 +32,17 @@
 
 namespace te
 {
+
+  namespace gm
+  {
+    class GeometryProperty;
+  }
+
   namespace ado
   {
 // Forward declaration
     class DataSourceTransactor;
-
+    
     /*!
       \class DataSourceCatalogLoader
 
@@ -75,6 +81,14 @@ namespace te
         te::da::Sequence* getSequence(const std::string& seqName);
 
         te::gm::Envelope* getExtent(const te::gm::GeometryProperty* gp);
+
+        /*!
+          \brief It load informations about the geometry property
+
+          \param dt The TerraLib DataSetType
+          \param geomp The TerraLib Geometry Property to load
+        */
+        void getGeometryColumn(te::da::DataSetType* dt, te::gm::GeometryProperty* geomp);
 
         void loadCatalog(const bool full = false);
 
