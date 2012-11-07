5b54ef05d4d61b4ac1284c1b3b13b7206fcc92c5
gribeiro <gribeiro@dpi.inpe.br>
2012-11-07 09:43:30 -0200

    added some initial files using xml api

---
+++ src/terralib/serialization/dataaccess/SQLDialect.cpp
@@ -0,0 +1,112 @@
+/*  Copyright (C) 2011-2011 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file SQLDialect.cpp
+   
+  \brief Auxiliary classes and functions to read SQL dialect information from a XML document.
+*/
+
+// TerraLib
+#include "../../xml/Reader.h"
+#include "../../xml/ReaderFactory.h"
+#include "../../dataaccess/query/BinaryOpEncoder.h"
+#include "../../dataaccess/query/FunctionEncoder.h"
+#include "../../dataaccess/query/SQLDialect.h"
+#include "../../dataaccess/query/SQLFunctionEncoder.h"
+#include "../../dataaccess/query/TemplateEncoder.h"
+#include "../../dataaccess/query/UnaryOpEncoder.h"
+#include "../Exception.h"
+#include "SQLDialect.h"
+
+// STL
+#include <cassert>
+#include <memory>
+
+// Boost
+#include <boost/format.hpp>
+
+te::da::SQLDialect* te::serialize::ReadDialect(const std::string& dialectFileName)
+{
+  std::auto_ptr<te::xml::Reader> xmlReader(te::xml::ReaderFactory::make());
+
+  xmlReader->read(dialectFileName);
+
+  if(!xmlReader->next())
+    throw Exception((boost::format(TR_DATAACCESS("Could not read query dialect information in file: %1%.")) % dialectFileName).str());
+
+  if(xmlReader->getNodeType() != te::xml::START_ELEMENT)
+    throw Exception((boost::format(TR_DATAACCESS("Error reading the document %1%, the start element wasn't found.")) % dialectFileName).str());
+
+  if(xmlReader->getElementLocalName() != "QueryDialect")
+    throw Exception((boost::format(TR_DATAACCESS("The first tag in the document %1% is not 'QueryDialect'.")) % dialectFileName).str());
+
+  xmlReader->next();
+  assert(xmlReader->getNodeType() == te::xml::START_ELEMENT);
+  assert(xmlReader->getElementLocalName() == "DriverName");
+
+  xmlReader->next();
+  assert(xmlReader->getNodeType() == te::xml::VALUE);
+
+  xmlReader->next();
+  assert(xmlReader->getNodeType() == te::xml::START_ELEMENT);
+  assert(xmlReader->getElementLocalName() == "Functions");
+
+  std::auto_ptr<te::da::SQLDialect> mydialect(new te::da::SQLDialect);
+
+  while(xmlReader->next() &&
+        (xmlReader->getNodeType() == te::xml::START_ELEMENT) &&
+        (xmlReader->getElementLocalName() == "Function"))
+  {
+    std::string fname = xmlReader->getAttr(0);
+
+    xmlReader->next();
+    assert(xmlReader->getNodeType() == te::xml::START_ELEMENT);
+
+    std::string encoderType = xmlReader->getElementLocalName();
+
+    te::da::SQLFunctionEncoder* sfe = 0;
+
+    if(encoderType == "FunctionEncoder")
+    {
+      std::string alias = xmlReader->getAttr(0);
+      sfe = new te::da::FunctionEncoder(alias);
+    }
+    else if(encoderType == "BinaryOpEncoder")
+    {
+      std::string alias = xmlReader->getAttr(0);
+      sfe = new te::da::BinaryOpEncoder(alias);
+    }
+    else if(encoderType == "UnaryOpEncoder")
+    {
+      std::string alias = xmlReader->getAttr(0);
+      sfe = new te::da::UnaryOpEncoder(alias);
+    }
+    else
+    {
+      throw Exception(TR_DATAACCESS("Unsupported encoder type!"));
+    }
+
+    mydialect->insert(fname, sfe);
+  }
+
+  return mydialect.release();
+}
+
+
---
+++ src/terralib/serialization/datatype/Property.cpp
@@ -0,0 +1,97 @@
+/*  Copyright (C) 2011-2011 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/serialization/datatype/Property.cpp
+ 
+  \brief Support for Property serialization.
+*/
+
+// TerraLib
+#include "../../common/Translator.h"
+#include "../../datatype/Enums.h"
+#include "../../datatype/NumericProperty.h"
+#include "../../datatype/Property.h"
+#include "../../datatype/SimpleProperty.h"
+#include "../../datatype/StringProperty.h"
+#include "../../geometry/GeometryProperty.h"
+#include "../../xml/Reader.h"
+#include "../../xml/Writer.h"
+#include "../Exception.h"
+#include "Property.h"
+
+// STL
+#include <cassert>
+
+te::dt::Property* te::serialize::ReadProperty(te::xml::Reader& reader)
+{
+  assert(reader.getNodeType() == te::xml::START_ELEMENT);
+  assert(reader.getElementLocalName() == "Property");
+
+  unsigned int id = reader.getAttrAsUInt32(0);
+  std::string name = reader.getAttr(1);
+  int datatype = reader.getAttrAsInt32(2);
+
+  reader.next();
+
+  te::dt::Property* p = 0;
+
+  switch(datatype)
+  {
+    case te::dt::CHAR_TYPE:
+    case te::dt::UCHAR_TYPE:
+    case te::dt::INT16_TYPE:
+    case te::dt::INT32_TYPE:
+    case te::dt::INT64_TYPE:
+    case te::dt::BOOLEAN_TYPE:
+    case te::dt::FLOAT_TYPE:
+    case te::dt::DOUBLE_TYPE:
+      p = new te::dt::SimpleProperty(name, datatype, false, 0, id, 0);
+    break;
+
+    case te::dt::NUMERIC_TYPE:
+      p = new te::dt::NumericProperty(name, 0, 0, false, 0, id, 0);
+    break;
+
+    case te::dt::STRING_TYPE:
+      p = new te::dt::StringProperty(name, te::dt::STRING, 0, false, 0, id, 0);
+    break;
+
+    case te::dt::GEOMETRY_TYPE:
+      p = new te::gm::GeometryProperty(name, 0, te::gm::GeometryType);
+    break;
+
+    default:
+      throw Exception(TR_SERIALIZATION("Data type not supported!"));
+  }
+
+  return p;
+}
+
+void te::serialize::Save(const te::dt::Property* p, te::xml::Writer& writer)
+{
+  writer.writeStartElement("Property");
+
+  writer.writeAttribute("id", p->getId());
+  writer.writeAttribute("name", p->getName());
+  writer.writeAttribute("datatype", p->getType());
+
+  writer.writeEndElement("Property");
+}
+
