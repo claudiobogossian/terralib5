7b0bdeb8d591174140b4c056ad13a1b9bc95fbb6
Lubia Vinhas <lubia@dhcp88.dpi.inpe.br>
2012-11-13 16:07:43 -0200

    to reflect changes in plugin support

--- examples/raster/LoadModules.cpp
+++ examples/raster/LoadModules.cpp
@@ -7,138 +7,63 @@
 
 void LoadModules()
 {
-    {
-#if TE_EXAMPLE_USE_OGR
-      te::plugin::PluginInfo info;
-
-#if TE_PLATFORM == TE_PLATFORMCODE_MSWINDOWS
-      info.m_type = "dll";
-#elif TE_PLATFORM == TE_PLATFORMCODE_LINUX
-      info.m_type = "s.o.";
-#elif TE_PLATFORM == TE_PLATFORMCODE_APPLE
-      info.m_type = "dylib";
-#else
-  #error "Platform not supported yet"
-#endif
-
-      info.m_name = "OGR DataSource Driver";
-      info.m_description = "This data source driver supports...";
-
-#if TE_PLATFORM == TE_PLATFORMCODE_MSWINDOWS
-#ifdef NDEBUG
-      info.m_mainFile = "terralib_ogr.dll";
-#else
-      info.m_mainFile = "terralib_ogr_d.dll";
-#endif
-#endif
-
-#if TE_PLATFORM == TE_PLATFORMCODE_LINUX
-#ifdef NDEBUG
-      info.m_mainFile = "libterralib_ogr.so";
-#else
-      info.m_mainFile = "libterralib_ogr_d.so";
-#endif
-#endif
-
-#if TE_PLATFORM == TE_PLATFORMCODE_APPLE
-#ifdef NDEBUG
-      info.m_mainFile = "libterralib_ogr.dylib";
-#else
-      info.m_mainFile = "libterralib_ogr_d.dylib";
-#endif
-#endif
-
-      te::plugin::PluginManager::getInstance().loadPlugin(info);
-#endif
-    }
-
-    {
-#if TE_EXAMPLE_USE_GDAL
-      te::plugin::PluginInfo info;
+  try
+  { 
+#if TE_EXAMPLE_USE_PGIS    
+    {      
+      te::plugin::PluginInfo info;      
+      info.m_name = "te.da.pgis";
+      info.m_displayName = "POSTGIS DataSource Driver";
+      info.m_description = "This data source driver supports data stored in a PostGIS database";
+      info.m_engine = "C++";
+      info.m_folder = PLUGINS_PATH;
       
-#if TE_PLATFORM == TE_PLATFORMCODE_MSWINDOWS
-      info.m_type = "dll";
-#elif TE_PLATFORM == TE_PLATFORMCODE_LINUX
-      info.m_type = "s.o.";
-#elif TE_PLATFORM == TE_PLATFORMCODE_APPLE
-      info.m_type = "dylib";
-#else
-  #error "Platform not supported yet"
-#endif
-
-      info.m_name = "GDAL DataSource Driver";
-      info.m_description = "This data source driver supports...";
-
-#if TE_PLATFORM == TE_PLATFORMCODE_MSWINDOWS
-#ifdef NDEBUG
-      info.m_mainFile = "terralib_gdal.dll";
-#else
-      info.m_mainFile = "terralib_gdal_d.dll";
-#endif
-#endif
-
-#if TE_PLATFORM == TE_PLATFORMCODE_LINUX
-#ifdef NDEBUG
-      info.m_mainFile = "libterralib_gdal.so";
-#else
-      info.m_mainFile = "libterralib_gdal_d.so";
-#endif
-#endif
-
-#if TE_PLATFORM == TE_PLATFORMCODE_APPLE
-#ifdef NDEBUG
-      info.m_mainFile = "libterralib_gdal.dylib";
-#else
-      info.m_mainFile = "libterralib_gdal_d.dylib";
-#endif
-#endif
-
-      te::plugin::PluginManager::getInstance().loadPlugin(info);
-#endif
+      std::pair<std::string, std::string> rsc("SharedLibraryName", "terralib_postgis");
+      
+      info.m_resources.push_back(rsc);
+      
+      te::plugin::PluginManager::getInstance().load(info);
     }
-
+#endif    
+    
+#if TE_EXAMPLE_USE_OGR  
     {
-#if TE_EXAMPLE_USE_GRIB
-      te::plugin::PluginInfo info;
-
-#if TE_PLATFORM == TE_PLATFORMCODE_MSWINDOWS
-      info.m_type = "dll";
-#elif TE_PLATFORM == TE_PLATFORMCODE_LINUX
-      info.m_type = "s.o.";
-#elif TE_PLATFORM == TE_PLATFORMCODE_APPLE
-      info.m_type = "dylib";
-#else
-  #error "Platform not supported yet"
-#endif
-
-      info.m_name = "GRIB DataSource Driver";
-      info.m_description = "This data source driver supports...";
-
-#if TE_PLATFORM == TE_PLATFORMCODE_MSWINDOWS
-#ifdef NDEBUG
-      info.m_mainFile = "terralib_grib.dll";
-#else
-      info.m_mainFile = "terralib_grib_d.dll";
-#endif
-#endif
-
-#if TE_PLATFORM == TE_PLATFORMCODE_LINUX
-#ifdef NDEBUG
-      info.m_mainFile = "libterralib_grib.so";
-#else
-      info.m_mainFile = "libterralib_grib.so";
-#endif
-#endif
-
-#if TE_PLATFORM == TE_PLATFORMCODE_APPLE
-#ifdef NDEBUG
-      info.m_mainFile = "libterralib_grib.dylib";
-#else
-      info.m_mainFile = "libterralib_grib_d.dylib";
-#endif
-#endif
-
-      te::plugin::PluginManager::getInstance().loadPlugin(info);
-#endif
+      te::plugin::PluginInfo info; 
+      info.m_name = "te.da.ogr";
+      info.m_displayName = "OGR DataSource Driver";
+      info.m_description = "This data source driver supports spatial data managed by OGR";
+      info.m_engine = "C++";
+      info.m_folder = PLUGINS_PATH;
+      
+      std::pair<std::string, std::string> rsc("SharedLibraryName", "terralib_ogr");
+      
+      info.m_resources.push_back(rsc);
+      
+      te::plugin::PluginManager::getInstance().load(info);
     }
+#endif
+    
+#if TE_EXAMPLE_USE_GDAL  
+    {
+      te::plugin::PluginInfo info; 
+      info.m_name = "te.da.gdal";
+      info.m_displayName = "GDAL DataSource Driver";
+      info.m_description = "This data source driver supports spatial data managed by GDAL";
+      info.m_engine = "C++";
+      info.m_folder = PLUGINS_PATH;
+      
+      std::pair<std::string, std::string> rsc("SharedLibraryName", "terralib_gdal");
+      
+      info.m_resources.push_back(rsc);
+      
+      te::plugin::PluginManager::getInstance().load(info);
+    }
+#endif    
+
+  }
+  catch(const te::common::Exception& e)
+  {
+    throw e;
+  }  
 }
+
