16132c4edd3ceb1cb5135fc8d27d7c9cf3193cf3
gribeiro <gribeiro@dpi.inpe.br>
2012-11-21 13:35:18 -0200

    added layer selector draft

---
+++ src/terralib/qt/widgets/layer/selector/core/AbstractLayerSelector.h
@@ -0,0 +1,71 @@
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/qt/widgets/layer/selector/core/AbstractLayerSelector.h
+
+  \brief ....
+*/
+
+#ifndef __TERRALIB_QT_WIDGETS_LAYER_SELECTOR_CORE_INTERNAL_ABSTRACTLAYERSELECTOR_H
+#define __TERRALIB_QT_WIDGETS_LAYER_SELECTOR_CORE_INTERNAL_ABSTRACTLAYERSELECTOR_H
+
+// TerraLib
+#include "../../../../../dataaccess/datasource/DataSourceInfo.h"
+#include "../../../../../maptools/forward.h"
+#include "../../../Config.h"
+
+// STL
+#include <list>
+
+// Qt
+#include <QtGui/QWidget>
+
+namespace te
+{
+  namespace qt
+  {
+    namespace widgets
+    {
+      /*!
+        \class AbstractLayerSelector
+
+        \brief ....
+      */
+      class TEQTWIDGETSEXPORT AbstractLayerSelector : public QWidget
+      {
+        Q_OBJECT
+
+        public:
+
+          AbstractLayerSelector(QWidget* parent = 0, Qt::WindowFlags f = 0);
+
+          ~AbstractLayerSelector();
+
+          virtual void set(const std::list<te::da::DataSourceInfoPtr>&) = 0;
+
+          virtual std::list<te::map::AbstractLayerPtr> getLayers() = 0;
+      }; 
+
+    } // end namespace widgets
+  }   // end namespace qt
+}     // end namespace te
+
+#endif  // __TERRALIB_QT_WIDGETS_LAYER_SELECTOR_CORE_INTERNAL_ABSTRACTLAYERSELECTOR_H
+
