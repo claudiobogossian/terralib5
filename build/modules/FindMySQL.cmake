6a5cf680c8db1ecb3a9247cfa984be3591622a73
frederico.bede <frederico.bede@funcate.org.br>
2012-12-10 16:13:30 -0200

    Renaming directories

---
+++ build/modules/FindMySQL.cmake
@@ -0,0 +1,28 @@
+cmake_minimum_required(VERSION 2.8)
+
+# Find path - tries to find *.h in paths hard-coded by the script
+FIND_PATH (MYSQL_INCLUDE_DIR driver/mysql_public_iface.h
+           PATHS /usr/local/include/mysql /usr/include/mysql /usr/local/mysql/include)
+
+# Find library - - tries to find *.a,*.so,*.dylib in paths hard-coded by the script
+
+FIND_LIBRARY(MYSQL_LIBRARY
+             NAMES libmysqlcppconn mysqlcppconn
+             PATHS /usr/local/lib /usr/lib /usr/local/mysql/lib)
+
+# Export include and library path for linking with other libraries
+
+SET(MYSQL_FOUND FALSE)
+
+IF (MYSQL_INCLUDE_DIR  AND MYSQL_LIBRARY)
+	SET(MYSQL_FOUND TRUE)
+  MESSAGE("-- Found MySQL library")
+ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+  if(MySQL_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "-- Error: MySQL required but NOT found.")
+  else()
+    MESSAGE(STATUS "-- Could NOT find MySQL library")
+  endif()
+ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+
+MARK_AS_ADVANCED( MYSQL_LIBRARY MYSQL_INCLUDE_DIR )
