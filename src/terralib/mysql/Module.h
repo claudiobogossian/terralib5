b91fa506db00c511cf909d4c2f107feb81ddf288
gribeiro <gribeiro@dpi.inpe.br>
2013-01-21 15:08:48 -0200

    merging with local branch

--- src/terralib/mysql/Module.h
+++ src/terralib/mysql/Module.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
 
     This file is part of the TerraLib - a Framework for building GIS enabled applications.
 
@@ -18,26 +18,31 @@
  */
 
 /*!
-  \file Module.h
+  \file terralib/mysql/Module.h
    
-  \brief The TerraLib OGR driver is a plugin.
-*/
+  \brief The TerraLib MySQL driver is a plugin.
+ */
 
-#ifndef __TERRALIB_OGR_INTERNAL_MODULE_H
-#define __TERRALIB_OGR_INTERNAL_MODULE_H
+#ifndef __TERRALIB_MYSQL_INTERNAL_MODULE_H
+#define __TERRALIB_MYSQL_INTERNAL_MODULE_H
 
 // TerraLib
 #include "../plugin/Plugin.h"
 #include "Config.h"
 
+// STL
+#include <string>
+
 namespace te
 {
-  namespace ogr
+  namespace mysql
   {
     /*!
       \class Module
 
-      \brief The TerraLib OGR driver is a plugin.    
+      \brief An utility class to initialize and terminate TerraLib MySQL driver support.
+
+      \sa te::plugin::Plugin
     */
     class Module : public te::plugin::Plugin
     {
@@ -53,47 +58,36 @@ namespace te
         /*! \brief Destructor. */
         ~Module();
 
-        /*!
-          \brief It initializes all TerraLib OGR driver support.       
+        /*! 
+          \brief It initializes the TerraLib MySQL driver support.       
            
           The initialization includes:
           <ul>
           <li>Multilanguage support</li>
-          <li>OGRRegisterAll</li>
+          <li>MySQL capabilities</li>
+          <li>MySQL query dialect</li>
           </ul>
            
           \exception Exception It may throws an exception.
          
-          \note This function must be called at least once in your program. The best
-                place to call it is inside the main routine of your application.
-
           \warning Not thread safe!
-
-          \warning If this method throws an exception we recommend you to
-                    quit the program, don't try to resume it because you can have intermittent errors!
         */
         void startup();
 
         /*!
-          \brief It finalizes all TerraLib OGR driver support.
+          \brief It finalizes the TerraLib MySQL driver support.
            
           \exception Exception It may throws an exception.
-           
-          \note This function must be called in your program at least the same times you have called
-                initialize. In general this must be the last call to TerraLib API. As in case of the initialize method,
-                this routine can be called at the end of the main routine of your application.
 
           \warning Not thread safe!
-
-          \warning If this method throws an exception we recommend you to
-                   quit the program, don't try to resume it because you can have intermittent errors!
         */
         void shutdown();
     };
-  } // end namespace ogr
+
+  } // end namespace mysql
 }   // end namespace te
 
-PLUGIN_CALL_BACK_DECLARATION(TEOGREXPORT);
+PLUGIN_CALL_BACK_DECLARATION(TEMYSQLEXPORT);
 
-#endif  // __TERRALIB_OGR_INTERNAL_MODULE_H
+#endif  // __TERRALIB_MYSQL_INTERNAL_MODULE_H
 
