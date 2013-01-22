7f8a8a76114158ba77890cf2e1740120bd5fb531
gribeiro <gribeiro@dpi.inpe.br>
2013-01-22 17:43:47 -0200

    treeitem is using the counted pattern

---
+++ src/terralib/common/TreeItem.cpp
@@ -0,0 +1,277 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/common/TreeItem.cpp
+
+  \brief A base class for data organized as a tree.
+*/
+
+// TerraLib
+#include "TreeItem.h"
+
+// STL
+#include <algorithm>
+#include <cassert>
+
+const std::size_t te::common::TreeItem::npos(-1);
+
+te::common::TreeItem::TreeItem(TreeItem* parent)
+  : m_parent(parent)
+{
+  if(m_parent)
+    m_parent->m_children.push_back(TreeItemPtr(this));
+}
+
+te::common::TreeItem::TreeItem(const TreeItemPtr& parent)
+{
+  if(parent.get())
+    m_parent->m_children.push_back(TreeItemPtr(this));
+}
+
+te::common::TreeItem::~TreeItem()
+{
+  iterator it = begin();
+  iterator itend = end();
+
+  while(it != itend)
+    (*it)->m_parent = 0;
+}
+
+te::common::TreeItem* te::common::TreeItem::getParent() const
+{
+  return m_parent;
+}
+
+const te::common::TreeItemPtr& te::common::TreeItem::getChild(std::size_t i) const
+{
+  assert(i < m_children.size());
+
+  const_iterator it = begin();
+
+  std::advance(it, i);
+
+  return *it;
+}
+
+const te::common::TreeItemPtr& te::common::TreeItem::operator[](std::size_t i) const
+{
+  return getChild(i);
+}
+
+void te::common::TreeItem::add(const TreeItemPtr& childItem)
+{
+  assert(childItem);
+
+  childItem->setParent(this);
+}
+
+void te::common::TreeItem::insert(std::size_t i, const TreeItemPtr& childItem)
+{
+  assert(i <= m_children.size());
+  assert(childItem);
+
+  childItem->disconnect();
+
+  childItem->m_parent = this;
+
+  iterator it = begin();
+
+  std::advance(it, i);
+
+  m_children.insert(it, childItem);
+}
+
+te::common::TreeItemPtr te::common::TreeItem::remove(std::size_t i)
+{
+  assert(i < m_children.size());
+
+  iterator it = begin();
+
+  std::advance(it, i);
+
+  TreeItemPtr childItem(*it);
+
+  childItem->disconnect();
+
+  m_children.erase(it);
+
+  return childItem;
+}
+
+std::list<te::common::TreeItemPtr> te::common::TreeItem::remove(std::size_t i, std::size_t count)
+{
+  assert(i < m_children.size());
+  assert(count > 0);
+  assert((i + count - 1) < m_children.size());
+
+  iterator firstIt = begin();
+
+  std::advance(firstIt, i);
+
+  iterator lastIt = firstIt;
+
+  std::advance(lastIt, count);
+
+  std::list<TreeItemPtr> removedItems;
+
+  for(iterator it = firstIt; it != lastIt; ++it)
+  {
+    TreeItemPtr childItem = *it;
+
+    childItem->disconnect();
+
+    removedItems.push_back(childItem);
+  }
+
+  m_children.erase(firstIt, lastIt);
+
+  return removedItems;
+}    
+
+te::common::TreeItemPtr te::common::TreeItem::replace(std::size_t i, const TreeItemPtr& childItem)
+{
+  assert(i < m_children.size());
+  assert(childItem);
+
+  TreeItemPtr replacedItem = getChild(i);
+  replacedItem->disconnect();
+
+  insert(i, childItem);
+
+  return replacedItem;
+}
+
+std::size_t te::common::TreeItem::getChildrenCount() const
+{
+  return m_children.size();
+}
+
+void te::common::TreeItem::getDescendantsCount(std::size_t& count) const
+{
+  count += m_children.size();
+
+  for(const_iterator it = begin(); it != end(); ++it)
+  {
+    TreeItemPtr item = *it;
+    item->getDescendantsCount(count);
+  }
+}
+
+void te::common::TreeItem::disconnect()
+{
+  if(m_parent == 0)
+    return;
+
+  int index = getIndex();
+
+  if(index < 0)
+    return;
+
+  iterator it = m_parent->begin();
+
+  std::advance(it, index);
+
+  m_parent->m_children.erase(it);
+
+  m_parent = 0;
+}
+
+std::size_t te::common::TreeItem::getIndex() const
+{
+  if(m_parent == 0)
+    return npos;
+
+  const std::list<TreeItemPtr>& children = m_parent->m_children;
+
+  std::size_t k = 0;
+
+  for(const_iterator it = children.begin(); it != children.end(); ++it)
+  {
+    TreeItemPtr childItem = *it;
+
+    if(childItem.get() == this)
+      return k;
+
+    ++k;
+  }
+
+  return npos;
+}
+
+void te::common::TreeItem::swap(const TreeItemPtr& firstChild, const TreeItemPtr& secondChild)
+{
+  assert(firstChild->getParent() == this && secondChild->getParent() == this);
+
+  std::size_t fidx = firstChild->getIndex();
+  std::size_t sidx = secondChild->getIndex();
+
+  iterator firstIt = begin();
+
+  std::advance(firstIt, fidx);
+
+  iterator secondIt = begin();
+
+  std::advance(secondIt, sidx);
+
+  *firstIt = secondChild;
+  *secondIt = firstChild;
+}
+
+void te::common::TreeItem::swap(const TreeItemPtr& sibling)
+{
+  assert(m_parent);
+
+  m_parent->swap(this, sibling);
+}
+
+te::common::TreeItem::const_iterator te::common::TreeItem::begin() const
+{
+  return m_children.begin();
+}
+
+te::common::TreeItem::iterator te::common::TreeItem::begin()
+{
+  return m_children.begin();
+}
+
+te::common::TreeItem::const_iterator te::common::TreeItem::end() const
+{
+  return m_children.end();
+}
+
+te::common::TreeItem::iterator te::common::TreeItem::end()
+{
+  return m_children.end();
+}
+
+void te::common::TreeItem::setParent(TreeItem* parent)
+{
+  if(m_parent == parent)
+    return;
+
+  if(m_parent)
+    disconnect();
+
+  m_parent = parent;
+
+  if(parent)
+    parent->m_children.push_back(this);
+}
+
--- src/terralib/common/TreeItem.h
+++ src/terralib/common/TreeItem.h
@@ -18,71 +18,72 @@
  */
 
 /*!
-  \file TreeItem.h
+  \file terralib/common/TreeItem.h
 
-  \brief This abstract class defines the interface to organize items in a tree-oriented way.  
- */
+  \brief A base class for data organized as a tree.
+*/
 
-#ifndef __TERRALIB_COMMON_INTERNAL_TREEITEM_H
-#define __TERRALIB_COMMON_INTERNAL_TREEITEM_H
+#ifndef __TERRALIB_COMMON_INTERNAL_COUNTEDTREEITEM_H
+#define __TERRALIB_COMMON_INTERNAL_COUNTEDTREEITEM_H
 
 // TerraLib
 #include "Config.h"
-#include "STLUtils.h"
+#include "Counted.h"
 
 // STL
-#include <cassert>
 #include <list>
 
 // Boost
-#include <boost/noncopyable.hpp>
+#include <boost/intrusive_ptr.hpp>
 
 namespace te
 {
   namespace common
   {
+// Forward declaration
+    class TreeItem;
+
+    typedef boost::intrusive_ptr<TreeItem> TreeItemPtr;
+
     /*!
       \class TreeItem
 
       \brief This abstract class describes a basic item to be organized in a tree-oriented way.
-      
+
       A tree item can have several descendants (children) and only one parent.
       This data structure will clear all the memory used by the items.
 
-      \sa Layer, Project
-     */
-    template<class T> class TreeItem : public boost::noncopyable
+      This implementation is based in the counted pattern, allowing to share the ownership of the items.
+
+      \sa AbstractLayer
+    */
+    class TECOMMONEXPORT TreeItem : public Counted
     {
       public:
 
-        typedef typename std::list<TreeItem*>::iterator iterator;
-        typedef typename std::list<TreeItem*>::const_iterator const_iterator;
-
-        /** @name Initializer Methods
-         *  Methods related to instantiation and destruction.
-         */
-        //@{
+        typedef std::list<TreeItemPtr>::iterator iterator;
+        typedef std::list<TreeItemPtr>::const_iterator const_iterator;
 
         /*!
           \brief It initializes a new item having a parent.
-          
+
           Its parent will have this new item attached as its child automatically.
 
           \param parent The parent item of this new item.
+        */
+        explicit TreeItem(TreeItem* parent = 0);
 
-          \note If a parent exists, it will take the ownership of this new item.
-         */
-        explicit TreeItem(TreeItem* parent);
+        /*!
+          \brief It initializes a new item having a parent.
+
+          Its parent will have this new item attached as its child automatically.
+
+          \param parent The parent item of this new item.
+        */
+        explicit TreeItem(const TreeItemPtr& parent);
 
         /*! \brief Virtual destructor. */
         virtual ~TreeItem();
-
-        //@}
-
-        /** @name Accessor methods
-         *  Methods used to get or set properties.
-         */
-        //@{
 
         /*!
           \brief It returns a pointer to the parent of this node.
@@ -90,8 +91,8 @@ namespace te
           \return It returns a pointer to the parent node.
 
           \note You can´t free the memory pointed by the returned pointer; otherwise, the result will be unpredictable.
-         */
-        TreeItem* getParent() const;        
+        */
+        TreeItem* getParent() const;
 
         /*!
           \brief It returns the n-th child.
@@ -99,10 +100,8 @@ namespace te
           \param i The child index.
 
           \return It returns a pointer to the specified child node.
-            
-          \note You can´t free the memory pointed by the returned pointer; otherwise, the result will be unpredictable.
-         */
-        TreeItem* getChild(std::size_t i) const;
+        */
+        const TreeItemPtr& getChild(std::size_t i) const;
 
         /*!
           \brief It returns the n-th child.
@@ -110,73 +109,55 @@ namespace te
           \param i The child index.
 
           \return It returns a pointer to the specified child node.
-            
-          \note You can´t free the memory pointed by the returned pointer; otherwise, the result will be unpredictable.
-         */
-        TreeItem* operator[](std::size_t i) const;
+        */
+        const TreeItemPtr& operator[](std::size_t i) const;
 
         /*!
-          \brief It adds the child item to the list of children of this item. If the child item already has a parent,
+          \brief It adds (appends) the item to the end of the children's list.
+
+          If the child item already has a parent,
           it will be disconnected from it and will be attached to this one.
 
           \param childItem The item to be added as a child of this item.
 
-          \note This item will take the ownership of the pointer.
-
-          \note Never pass a NULL pointer to this method.
-         */
-        void push_back(TreeItem* childItem);
+          \pos The children's list will be increased with one element.
+        */
+        void add(const TreeItemPtr& childItem);
 
         /*!
-          \brief It adds(appends) a item in the end of the list of children. If the item has a parent,
-                 it will be disconnected from it and will be attached to this one.
+          \brief It inserts an item in the informed position.
 
-          \param item The item to be added.
-
-          \note This item will take the ownership of the pointer of the given item.
-
-          \note Never pass a NULL pointer to this method.
-         */
-        void addChild(TreeItem* item);
-
-        /*!
-          \brief It inserts a item in the position. If the item has a parent, 
-           it will be disconnected from it and will be attached to this one.
+          If the item has a parent,
+          it will be disconnected from it and will be attached to this one.
 
           \param i    The position where the item will be inserted.
           \param item The item to be inserted.
 
-          \note This item will take the ownership of the pointer of the given item.
-
-          \note Never pass a NULL pointer to this method.
-         */
-        void insertChild(std::size_t pos, TreeItem* item);
+          \pos The children's list will be increased with one element.
+        */
+        void insert(std::size_t i, const TreeItemPtr& childItem);
 
         /*!
-          \brief It takes the child in the i-th position by disconnecting it from the list of children.
+          \brief It removes the i-th child.
 
-          \param i The position of the item to be taken.
+          \param i The position of the item to be removed.
 
-          \return The item taken.
+          \return The removed item.
 
-          \note The caller will get the ownership of the node taken.
-
-         */
-        TreeItem* takeChild(std::size_t i);
+          \pos The children's list will be decreased by one element.
+        */
+        TreeItemPtr remove(std::size_t i);
 
         /*!
-          \brief It takes from the given position a certain number of items.
-                 The items to be taken will be disconnected from this item.
+          \brief It removes from the given position a certain number of items as children of this item.
+                 The items to be removed will be disconnected from this item.
 
-          \param i     The position from where the items will be taken as children of this item.
-          \param count The number of items to be removed as children of this item.
+          \param i     The position from where the items will be removed.
+          \param count The number of items to be removed (> 0).
 
-          \return The list of items that was taken.
-
-          \note The caller will get the ownership of the nodes that were taken.
-
-         */
-        std::list<TreeItem*> takeChildren(std::size_t i, std::size_t count);        
+          \return The list of removed items.
+        */
+        std::list<TreeItemPtr> remove(std::size_t i, std::size_t count);
 
         /*!
           \brief It replaces the child item at the given position by the new one.
@@ -186,16 +167,14 @@ namespace te
           \param childItem The new child item that will replace the item in the i-th position. 
 
           \return The item that was replaced.
-
-          \note The caller will get the ownership of the replaced item. 
-         */
-        TreeItem* replace(std::size_t i, TreeItem* childItem);
+        */
+        TreeItemPtr replace(std::size_t i, const TreeItemPtr& childItem);
 
         /*!
           \brief It returns the number of children of this node.
 
           \return The number of direct descendants of this node.
-         */
+        */
         std::size_t getChildrenCount() const;
 
         /*!
@@ -207,344 +186,86 @@ namespace te
                 how many elements there are in a tree, just call this method for the root item.
 
           \note The count parameter must be initialized with zero.
-         */
+        */
         void getDescendantsCount(std::size_t& count) const;
 
-        /*! \brief It disconnects this item from its parent, if it has one.
-        
-            \note The caller wiil get the ownership of this item. 
-        */
+        /*! \brief It disconnects this item from its parent, if it has one. */
         void disconnect();
 
         /*!
           \brief It returns the index of this item in the list of children of its parent item.
 
           \return The index of this item in the list of children of its parent item. It returns
-                  a negative value (-1), if it has no parent.
-         */
-        int getIndex() const;
+                  npos, if it has no parent.
+        */
+        std::size_t getIndex() const;
 
         /*!
           \brief It swaps the position of the given children.
 
           \param firstChild  One of the children.
           \param secondChild The other child.
-         */
-        void swap(TreeItem* firstChild, TreeItem* secondChild);
+        */
+        void swap(const TreeItemPtr& firstChild, const TreeItemPtr& secondChild);
 
         /*!
           \brief It swaps this item with its sibling in their parent node.
 
           \param sibling The sibling to swap.
-         */
-        void swap(TreeItem* sibling);
+        */
+        void swap(const TreeItemPtr& sibling);
 
         /*!
           \brief It returns the constant iterator associated to the first child of this item.
 
           \return The constant iterator associated to the first child of this item.
-         */
+        */
         const_iterator begin() const;
-        
+
         /*!
           \brief It returns the iterator associated to the first child of this item.
 
           \return The iterator associated to the first child of this item.
-         */
+        */
         iterator begin();
 
         /*!
           \brief It returns the constant iterator that refers to one past the end of the children of this item.
 
           \return The constant iterator that refers to one past the end of the children of this item.
-         */
+        */
         const_iterator end() const;
-        
-       /*!
+
+        /*!
           \brief It returns the iterator that refers to one past the children of this item.
 
           \return The iterator that refers to one past the children of this item.
-         */
+        */
         iterator end();
-        
-        //@}
 
       protected:
 
-        /** @name Auxiliary Methods
-         */
-        //@{
-
         /*!
-          \brief It sets the parent of this item to the specified one. Don´t use this method if
+          \brief It sets the parent of this item to the specified one. Don't use this method if
                  you are not sure how to handle all the pointers involved in this operation.
 
           \param parent The item to be set as parent of this item.
 
-          \note Avoid calling this method; instead, use addChild and the constructors.
-
-          \note TreeItem will take the ownership of the pointer.
-         */
+          \note Avoid calling this method; instead, use add or the constructors.
+        */
         void setParent(TreeItem* parent);
 
-        //@}
+      public:
+
+        static const std::size_t npos;  //!< A value to indicate no match.
 
       protected:
 
-        TreeItem* m_parent;                //!< A pointer to the parent node.
-        std::list<TreeItem*> m_children;   //!< Pointers to the items that are children.
+        TreeItem* m_parent;                  //!< A pointer to the parent node.
+        std::list<TreeItemPtr> m_children;   //!< Pointers to the items that are children.
     };
-
-    template<class T> inline
-    TreeItem<T>::TreeItem(TreeItem<T>* parent)
-      : m_parent(parent)
-    {
-      if(m_parent)
-        m_parent->m_children.push_back(this);
-    }
-
-    template<class T> inline
-    TreeItem<T>::~TreeItem()
-    {
-      FreeContents(m_children);
-    }
-
-    template<class T> inline
-    TreeItem<T>* TreeItem<T>::getParent() const
-    {
-      return m_parent;
-    }    
-
-    template<class T> inline
-    TreeItem<T>* TreeItem<T>::getChild(std::size_t i) const
-    {
-      assert(i < m_children.size());
-
-      const_iterator it = begin();
-      std::advance(it, i);
-
-      return *it;
-    }
-
-    template<class T> inline
-    TreeItem<T>* TreeItem<T>::operator[](std::size_t i) const
-    {
-      return getChild(i);
-    }
-
-    template<class T> inline
-    void TreeItem<T>::push_back(TreeItem<T>* childItem)
-    {
-      assert(childItem);
-
-      childItem->setParent(this);
-    }
-
-    template<class T> inline
-    void TreeItem<T>::addChild(TreeItem<T>* item)
-    {
-      push_back(item);
-    }
-
-    template<class T> inline
-    void TreeItem<T>::insertChild(std::size_t pos, TreeItem<T>* item)
-    {
-      assert(pos <= m_children.size());
-      assert(item);
-
-      item->disconnect();
-
-      item->m_parent = this;
-
-      iterator it = begin();
-      std::advance(it, pos);
-
-      m_children.insert(it, item); 
-    }
-
-    template<class T> inline
-    TreeItem<T>* TreeItem<T>::takeChild(std::size_t i)
-    {
-      assert(i < m_children.size());
-
-      iterator it = begin();
-
-      std::advance(it, i);
-
-      TreeItem* item = *it;
-      item->m_parent = 0;
-
-      m_children.erase(it);
-
-      return item;
-    } 
-
-    template<class T> inline
-    std::list<TreeItem<T>*> TreeItem<T>::takeChildren(std::size_t i, std::size_t count)
-    {
-      assert(i < m_children.size());
-
-      iterator it, firstIt, lastIt;
-
-      firstIt = begin();
-      std::advance(firstIt, i);
-
-      lastIt = firstIt;
-      std::advance(lastIt, count);
-
-      std::list<TreeItem*> itemsList;
-      for (it = firstIt; it != lastIt; ++it)
-      {
-        TreeItem* item = *it;
-        item->m_parent = 0;
-        itemsList.push_back(item);
-      }
-
-      m_children.erase(firstIt, lastIt);
-
-      return itemsList;
-    }    
-
-    template<class T> inline
-    TreeItem<T>* TreeItem<T>::replace(std::size_t i, TreeItem<T>* childItem)
-    {
-      assert(i < (int)m_children.size());
-      assert(childItem);
-
-      TreeItem* replacedItem = getChild(i);
-      replacedItem->disconnect();
-
-      insertChild(i, childItem);
-
-      return replacedItem;
-    }
-
-    template<class T> inline
-    std::size_t TreeItem<T>::getChildrenCount() const
-    {
-      return m_children.size();
-    }
-
-    template<class T> inline
-    void TreeItem<T>::getDescendantsCount(std::size_t& count) const
-    {
-      count += m_children.size();
-
-      const_iterator it;
-      for (it = begin(); it != end(); ++it)
-      {
-        TreeItem* item = *it;
-        item->getDescendantsCount(count);
-      }
-    }
-
-    template<class T> inline
-    void TreeItem<T>::disconnect()
-    {
-      if (m_parent == 0)
-        return;
-
-      int index = getIndex();
-      if (index < 0)
-        return;
-
-      iterator it = m_parent->begin();
-      std::advance(it, index);
-
-      m_parent->m_children.erase(it);
-      m_parent = 0;
-    }
-
-    template<class T> inline
-    int TreeItem<T>::getIndex() const
-    {
-      if (m_parent == 0)
-        return -1;
-
-      std::list<TreeItem*>& children = m_parent->m_children;
-
-      const_iterator it;
-      int k = 0;
-
-      for (it = children.begin(); it != children.end(); ++it)
-      {
-        TreeItem* item = *it;
-
-        if (item == this)
-          break;
-
-        ++k;
-      }  
-
-      return k;
-    }
-
-    template<class T> inline
-    void TreeItem<T>::swap(TreeItem<T>* firstChild, TreeItem<T>* secondChild)
-    {
-      assert(firstChild->getParent() == this &&
-             secondChild->getParent() == this);
-
-      std::size_t fidx = firstChild->getIndex();
-      std::size_t sidx = secondChild->getIndex();
-
-      iterator firstIt = begin();
-      std::advance(firstIt, fidx);
-
-      iterator secondIt = begin();
-      std::advance(secondIt, sidx);
-
-      *firstIt = secondChild;
-      *secondIt = firstChild;
-    }
-
-    template<class T> inline
-    void TreeItem<T>::swap(TreeItem<T>* sibling)
-    {
-      if (m_parent)
-        m_parent->swap(this, sibling);
-    }
-
-    template<class T> inline
-    typename TreeItem<T>::const_iterator TreeItem<T>::begin() const
-    {
-      return m_children.begin();
-    }
-    
-    template<class T> inline
-    typename TreeItem<T>::iterator TreeItem<T>::begin()
-    {
-      return m_children.begin();
-    }
-
-    template<class T> inline
-    typename TreeItem<T>::const_iterator TreeItem<T>::end() const
-    {
-      return m_children.end();
-    }
-    
-    template<class T> inline
-    typename TreeItem<T>::iterator TreeItem<T>::end()
-    {
-      return m_children.end();
-    }
-
-    template<class T> inline
-    void TreeItem<T>::setParent(TreeItem<T>* parent)
-    {
-      if (m_parent == parent)
-        return;
-
-      if (m_parent)
-        disconnect();
-
-      m_parent = parent;
-
-      if (parent)
-        parent->m_children.push_back(this);
-    }
 
   } // end namespace common
 }   // end namespace te
 
-#endif  // __TERRALIB_COMMON_INTERNAL_TREEITEM_H
+#endif  // __TERRALIB_COMMON_INTERNAL_COUNTEDTREEITEM_H
