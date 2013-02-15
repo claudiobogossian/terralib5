e4ca925b5259dc50d185114979195fc9fcd74d7c
gribeiro <gribeiro@dpi.inpe.br>
2013-02-15 10:20:19 -0200

    adding mysql connector

---
+++ src/terralib/qt/plugins/datasource/mysql/MySQLConnectorDialog.h
@@ -0,0 +1,94 @@
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/qt/widgets/connector/mysql/MySQLConnectorDialog.h
+
+  \brief ....
+*/
+
+#ifndef __TERRALIB_QT_WIDGETS_CONNECTOR_MYSQL_INTERNAL_MYSQLCONNECTORDIALOG_H
+#define __TERRALIB_QT_WIDGETS_CONNECTOR_MYSQL_INTERNAL_MYSQLCONNECTORDIALOG_H
+
+// TerraLib
+#include "../../../../../dataaccess/datasource/DataSource.h"
+#include "../../../../../dataaccess/datasource/DataSourceInfo.h"
+#include "../../../Config.h"
+
+// STL
+#include <memory>
+
+// Qt
+#include <QtGui/QDialog>
+
+namespace Ui { class MySQLConnectorDialogForm; }
+
+namespace te
+{
+  namespace qt
+  {
+    namespace widgets
+    {
+      /*!
+        \class MySQLConnectorDialog
+
+        \brief ....
+      */
+      class TEQTWIDGETSEXPORT MySQLConnectorDialog : public QDialog
+      {
+        Q_OBJECT
+
+        public:
+
+          MySQLConnectorDialog(QWidget* parent = 0, Qt::WindowFlags f = 0);
+
+          ~MySQLConnectorDialog();
+
+          const te::da::DataSourceInfoPtr& getDataSource() const;
+
+          const te::da::DataSourcePtr& getDriver() const;
+
+          void set(const te::da::DataSourceInfoPtr& ds);
+
+        public slots:
+
+          void openPushButtonPressed();
+
+          void testPushButtonPressed();
+
+          void helpPushButtonPressed();
+
+        protected:
+
+          void getConnectionInfo(std::map<std::string, std::string>& connInfo) const;
+
+          void setConnectionInfo(const std::map<std::string, std::string>& connInfo);
+
+        private:
+
+          std::auto_ptr<Ui::MySQLConnectorDialogForm> m_ui;
+          te::da::DataSourceInfoPtr m_datasource;
+          te::da::DataSourcePtr m_driver;
+      }; 
+    } // end namespace widgets
+  }   // end namespace qt
+}     // end namespace te
+
+#endif  // __TERRALIB_QT_WIDGETS_CONNECTOR_MYSQL_INTERNAL_MYSQLCONNECTORDIALOG_H
+
