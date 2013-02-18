f18178241b37b8f618d33b41ecc72dc53bc3bda2
felipe.siqueira <felipe.siqueira@funcate.org.br>
2013-02-18 11:07:23 -0300

    mysql qt plugin default off

--- build/terralib_qt_plugins/TerraLib_Plugins.cmake
+++ build/terralib_qt_plugins/TerraLib_Plugins.cmake
@@ -2,7 +2,7 @@ option (BUILD_QT_PLUGIN_DATASOURCE_GDAL "Build GDAL datasource datasource plugin
 option (BUILD_QT_PLUGIN_DATASOURCE_GEOFILE "Build GeoFile datasource plugin for Qt." OFF)
 option (BUILD_QT_PLUGIN_DATASOURCE_OGR "Build OGR datasource plugin for Qt." ON)
 option (BUILD_QT_PLUGIN_DATASOURCE_PGIS "Build PostGIS datasource plugin for Qt." ON)
-option (BUILD_QT_PLUGIN_DATASOURCE_MYSQL "Build PostGIS datasource plugin for Qt." ON)
+option (BUILD_QT_PLUGIN_DATASOURCE_MYSQL "Build PostGIS datasource plugin for Qt." OFF)
 option (BUILD_QT_PLUGIN_DATASOURCE_SHP "Build Shapefile datasource plugin for Qt." ON)
 option (BUILD_QT_PLUGIN_DATASOURCE_WCS "Build WCS datasource plugin for Qt." OFF)  
 option (BUILD_QT_PLUGIN_DATASOURCE_WFS "Build WFS datasource plugin for Qt." OFF)
