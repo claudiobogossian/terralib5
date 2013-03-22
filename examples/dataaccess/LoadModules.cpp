d377dd9cff695adea93a08e731df98dde73f6e8a
Douglas Uba <douglas@dpi.inpe.br>
2013-03-22 16:31:37 -0300

    Updating to use new name and extension of plugin files;

--- examples/dataaccess/LoadModules.cpp
+++ examples/dataaccess/LoadModules.cpp
@@ -13,27 +13,27 @@ void LoadModules()
   te::plugin::PluginInfo* info;
   
 #if TE_EXAMPLE_USE_OGR
-  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/plugin_ogr_info.xml"));
+  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.ogr.teplg"));
   te::plugin::PluginManager::getInstance().add(info); 
 #endif
   
 #if TE_EXAMPLE_USE_GDAL
-  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/plugin_gdal_info.xml"));
+  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.gdal.teplg"));
   te::plugin::PluginManager::getInstance().add(info);
 #endif
   
 #if TE_EXAMPLE_USE_PGIS
-  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/plugin_pgis_info.xml"));
+  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.pgis.teplg"));
   te::plugin::PluginManager::getInstance().add(info);    
 #endif
 
 #if TE_EXAMPLE_USE_MYSQL
-  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/plugin_mysql_info.xml"));
+  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.mysql.teplg"));
   te::plugin::PluginManager::getInstance().add(info);    
 #endif
 
 #if TE_EXAMPLE_USE_SQLITE
-  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/plugin_sqlite_info.xml"));
+  info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.sqlite.teplg"));
   te::plugin::PluginManager::getInstance().add(info);
 #endif   
   
