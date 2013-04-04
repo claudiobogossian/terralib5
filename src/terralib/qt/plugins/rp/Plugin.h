aebc04c24307ac5083fd8393278a19e2544307ec
Eric Silva Abreu <eric@dpi.inpe.br>
2013-04-04 13:52:59 -0300

    Added Raster Processing Qt Plugin... ApplicationController added method to get the current project.

---
+++ src/terralib/qt/plugins/rp/Plugin.h
@@ -0,0 +1,87 @@
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/qt/plugins/rp/Plugin.h
+
+  \brief Plugin implementation for the RP Qt Plugin widget.
+*/
+
+#ifndef __TE_QT_PLUGINS_RP_INTERNAL_PLUGIN_H
+#define __TE_QT_PLUGINS_RP_INTERNAL_PLUGIN_H
+
+// TerraLib
+#include "../../../plugin/Plugin.h"
+#include "Config.h"
+
+// Qt
+#include <QtGui/QMenu>
+
+namespace te
+{
+  namespace qt
+  {
+    namespace plugins
+    {
+      namespace rp
+      {
+        class ContrastAction;
+
+        class Plugin : public te::plugin::Plugin
+        {
+          public:
+
+            Plugin(const te::plugin::PluginInfo& pluginInfo);
+
+            ~Plugin();
+
+            void startup();
+
+            void shutdown();
+
+          protected:
+
+            /*!
+              \brief Function used to register all raster processing actions.
+
+            */
+            void registerActions();
+
+            /*!
+              \brief Function used to unregister all raster processing actions.
+
+            */
+            void unRegisterActions();
+
+          protected:
+
+            QMenu* m_rpMenu;                                    //!< RP Main Menu registered.
+
+            te::qt::plugins::rp::ContrastAction* m_contrast;    //!< Contrast Process Action
+
+        };
+
+      } // end namespace rp
+    }   // end namespace plugins
+  }     // end namespace qt
+}       // end namespace te
+
+PLUGIN_CALL_BACK_DECLARATION(TEQTPLUGINRPEXPORT);
+
+#endif //__TE_QT_PLUGINS_RP_INTERNAL_PLUGIN_H
