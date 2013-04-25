2cea1e4405e3adc4eae772db3c924b3ddcf4f4eb
gribeiro <gribeiro@dpi.inpe.br>
2013-04-25 10:03:43 -0300

    fixing build

--- src/terralib/qt/plugins/rp/ContrastAction.cpp
+++ src/terralib/qt/plugins/rp/ContrastAction.cpp
@@ -49,7 +49,7 @@ void te::qt::plugins::rp::ContrastAction::onActionActivated(bool checked)
   te::qt::widgets::ContrastWizard dlg(0);
 
 // get the list of layers from current project
-  te::qt::af::Project* prj = te::qt::af::ApplicationController::getInstance().get();
+  te::qt::af::Project* prj = te::qt::af::ApplicationController::getInstance().getProject();
 
   if(prj)
   {
