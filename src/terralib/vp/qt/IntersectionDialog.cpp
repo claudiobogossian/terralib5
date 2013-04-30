ce46f67999f1ff5bb344bd80272b1ef8eea98a02
felipe.siqueira <felipe.siqueira@funcate.org.br>
2013-04-30 13:51:34 -0300

    VP output datasource

--- src/terralib/vp/qt/IntersectionDialog.cpp
+++ src/terralib/vp/qt/IntersectionDialog.cpp
@@ -26,7 +26,10 @@
 // TerraLib
 #include "../../common/Translator.h"
 #include "../../dataaccess/dataset/DataSetType.h"
+#include "../../dataaccess/datasource/DataSourceInfo.h"
+#include "../../dataaccess/datasource/DataSourceInfoManager.h"
 #include "../../maptools/AbstractLayer.h"
+#include "../../qt/widgets/datasource/selector/DataSourceSelectorDialog.h"
 #include "../core/Exception.h"
 #include "IntersectionDialog.h"
 #include "LayerTreeModel.h"
@@ -35,6 +38,7 @@
 
 // Qt
 #include <QtGui/QTreeWidget>
+#include <QtGui/QFileDialog>
 
 te::vp::IntersectionDialog::IntersectionDialog(QWidget* parent, Qt::WindowFlags f)
   : QDialog(parent, f),
@@ -48,11 +52,14 @@ te::vp::IntersectionDialog::IntersectionDialog(QWidget* parent, Qt::WindowFlags 
   m_ui->m_imgLabel->setPixmap(QIcon::fromTheme(VP_IMAGES"/vp-intersection-hint").pixmap(48,48));
   m_ui->m_targetDatasourceToolButton->setIcon(QIcon::fromTheme("datasource"));
 
-  connect(m_ui->m_filterLineEdit, SIGNAL(textChanged(const QString&)), SLOT(onFilterLineEditTextChanged(const QString&)));
+  connect(m_ui->m_filterLineEdit, SIGNAL(textChanged(const QString&)), this, SLOT(onFilterLineEditTextChanged(const QString&)));
+  connect(m_ui->m_targetDatasourceToolButton, SIGNAL(pressed()), this, SLOT(onTargetDatasourceToolButtonPressed()));
+  connect(m_ui->m_targetFileToolButton, SIGNAL(pressed()), this,  SLOT(onTargetFileToolButtonPressed()));
 }
 
 te::vp::IntersectionDialog::~IntersectionDialog()
 {
+  delete m_model;
 }
 
 void te::vp::IntersectionDialog::setLayers(std::list<te::map::AbstractLayerPtr> layers)
@@ -89,7 +96,34 @@ int te::vp::IntersectionDialog::getMemoryUse()
     return LOW_MEM;
 }
 
-std::map<te::map::AbstractLayerPtr, std::vector<te::dt::Property*>> te::vp::IntersectionDialog::getSeleted()
+void te::vp::IntersectionDialog::onTargetDatasourceToolButtonPressed()
 {
-  return m_model->getSelected();
+  te::qt::widgets::DataSourceSelectorDialog dlg(this);
+  dlg.exec();
+
+  std::list<te::da::DataSourceInfoPtr> dsPtrList = dlg.getSelecteds();
+
+  if(dsPtrList.size() <= 0)
+    return;
+
+  std::list<te::da::DataSourceInfoPtr>::iterator it = dsPtrList.begin();
+
+  m_ui->m_repositoryLineEdit->setText(QString(it->get()->getTitle().c_str()));
+
+  m_outputDatasource = *it;
+}
+
+void te::vp::IntersectionDialog::onTargetFileToolButtonPressed()
+{
+  QString fileName = QFileDialog::getSaveFileName(this, tr("Open Feature File"), QString(""), tr("Common Formats (*.shp *.SHP *.kml *.KML *.geojson *.GEOJSON *.gml *.GML);; Shapefile (*.shp *.SHP);; GML (*.gml *.GML);; Web Feature Service - WFS (*.xml *.XML *.wfs *.WFS);; All Files (*.*)"), 0, QFileDialog::ReadOnly);
+  
+  if(fileName.isEmpty())
+    return;
+
+  m_ui->m_repositoryLineEdit->setText(fileName);
+
+  std::vector<te::da::DataSourceInfoPtr> datasources;
+  te::da::DataSourceInfoManager::getInstance().getByType("OGR", datasources);
+
+  m_outputDatasource = datasources[0];
 }
