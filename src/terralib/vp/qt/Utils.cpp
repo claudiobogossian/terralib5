fb2f14790a0e1903aab87eb290d0fefa000330bb
felipe.siqueira <felipe.siqueira@funcate.org.br>
2013-05-02 17:27:10 -0300

    VP improvements Add Utils Correct ++i Add Tree Filter

---
+++ src/terralib/vp/qt/Utils.cpp
@@ -0,0 +1,47 @@
+/*  Copyright (C) 2008-2011 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/vp/qt/Utils.cpp
+   
+  \brief Utility functions for Vector Processing.
+*/
+
+// TerraLib
+#include "../../common/StringUtils.h"
+#include "LayerTreeModel.h"
+#include "Utils.h"
+
+// Qt
+#include <QtGui/QTreeView>
+
+std::list<te::map::AbstractLayerPtr> te::vp::GetFilteredLayers(std::string text, std::list<te::map::AbstractLayerPtr> layers)
+{
+  std::list<te::map::AbstractLayerPtr> filteredList;
+
+  for(std::list<te::map::AbstractLayerPtr>::iterator it = layers.begin(); it != layers.end(); ++it)
+  {
+    std::string layName = (*it)->getTitle().substr(0, text.size());
+    
+    if(te::common::Convert2UCase(layName) == te::common::Convert2UCase(text))
+      filteredList.push_back(*it);
+  }
+
+  return filteredList;
+}
