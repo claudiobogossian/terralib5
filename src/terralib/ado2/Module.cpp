27bedab7eebc5d94c6c81b374b45aa8bae13ec76
felipe.siqueira <felipe.siqueira@funcate.org.br>
2013-07-24 15:43:46 -0300

    Added ado2 folder

---
+++ src/terralib/ado2/Module.cpp
@@ -0,0 +1,87 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+// TerraLib
+#include "../common/Logger.h"
+#include "../common/Translator.h"
+#include "../dataaccess/datasource/DataSourceCapabilities.h"
+#include "../dataaccess/datasource/DataSourceManager.h"
+#include "../dataaccess/query/SQLDialect.h"
+#include "../dataaccess/serialization/xml/Serializer.h"
+
+#include "Globals.h"
+#include "Module.h"
+
+// Boost
+#include <boost/filesystem.hpp>
+
+const std::string te::ado::Module::sm_unknownTypeName("UNKNOWN");
+const std::string te::ado::Module::sm_int2TypeName("INT2");
+const std::string te::ado::Module::sm_intTypeName("INT");
+const std::string te::ado::Module::sm_int8TypeName("INT8");
+const std::string te::ado::Module::sm_numericTypeName("NUMERIC");
+const std::string te::ado::Module::sm_dateTypeName("DATE");
+const std::string te::ado::Module::sm_timeTypeName("TIME");
+const std::string te::ado::Module::sm_timeTZTypeName("TIMETZ");
+const std::string te::ado::Module::sm_timeStampTypeName("TIMESTAMP");
+const std::string te::ado::Module::sm_timeStampTZTypeName("TIMESTAMPTZ");
+const std::string te::ado::Module::sm_floatTypeName("FLOAT4");
+const std::string te::ado::Module::sm_doubleTypeName("FLOAT8");
+const std::string te::ado::Module::sm_booleanTypeName("BOOL");
+const std::string te::ado::Module::sm_charTypeName("CHAR");
+const std::string te::ado::Module::sm_varcharTypeName("VARCHAR");
+const std::string te::ado::Module::sm_stringTypeName("TEXT");
+const std::string te::ado::Module::sm_byteArrayTypeName("BYTEA");
+const std::string te::ado::Module::sm_pointTypeName("POINT");
+const std::string te::ado::Module::sm_pointMTypeName("POINTM");
+const std::string te::ado::Module::sm_lineStringTypeName("LINESTRING");
+const std::string te::ado::Module::sm_lineStringMTypeName("LINESTRINGM");
+const std::string te::ado::Module::sm_polygonTypeName("POLYGON");
+const std::string te::ado::Module::sm_polygonMTypeName("POLYGONM");
+const std::string te::ado::Module::sm_geometryCollectionTypeName("GEOMETRYCOLLECTION");
+const std::string te::ado::Module::sm_geometryCollectionMTypeName("GEOMETRYCOLLECTIONM");
+const std::string te::ado::Module::sm_multiPointTypeName("MULTIPOINT");
+const std::string te::ado::Module::sm_multiPointMTypeName("MULTIPOINTM");
+const std::string te::ado::Module::sm_multiLineStringTypeName("MULTILINESTRING");
+const std::string te::ado::Module::sm_multiLineStringMTypeName("MULTILINESTRINGM");
+const std::string te::ado::Module::sm_multiPolygonTypeName("MULTIPOLYGON");
+const std::string te::ado::Module::sm_multiPolygonMTypeName("MULTIPOLYGONM");
+const std::string te::ado::Module::sm_geometryTypeName("GEOMETRY");
+
+te::ado::Module::Module(const te::plugin::PluginInfo& pluginInfo)
+  : te::plugin::Plugin(pluginInfo)
+{
+}
+
+te::ado::Module::~Module()
+{
+}
+
+void te::ado::Module::startup()
+{
+
+}
+
+void te::ado::Module::shutdown()
+{
+
+}
+
+PLUGIN_CALL_BACK_IMPL(te::ado::Module)
+
