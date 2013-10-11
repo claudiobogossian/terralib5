385f432d47cdef68dd536f44be5c912a550a356e
felipe.siqueira <felipe.siqueira@funcate.org.br>
2013-10-11 13:23:02 -0300

    ref #547 ref #548

---
+++ src/terralib/qt/plugins/terralib4/TL4ConverterWizard.cpp
@@ -0,0 +1,94 @@
+/*  Copyright (C) 2011-2012 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/qt/plugins/terralib4/TL4ConverterWizard.cpp
+
+  \brief A Qt dialog for ....
+*/
+
+// TerraLib
+#include "TL4ConverterWizard.h"
+#include "TL4ConnectorWizardPage.h"
+#include "TL4LayerSelectionWizardPage.h"
+#include "ui_TL4ConverterWizardForm.h"
+#include "ui_TL4ConnectorWizardPageForm.h"
+#include "ui_TL4LayerSelectionWizardPageForm.h"
+
+// STL
+#include <cassert>
+
+// Qt
+#include <QtGui/QAbstractButton>
+#include <QtGui/QMessageBox>
+#include <QtGui/QVBoxLayout>
+
+te::qt::plugins::terralib4::TL4ConverterWizard::TL4ConverterWizard(QWidget* parent, Qt::WindowFlags f)
+  : QWizard(parent, f),
+    m_ui(new Ui::TL4ConverterWizardForm)
+{
+// setup controls
+  m_ui->setupUi(this);
+
+// add pages
+  m_connectorPage.reset(new TL4ConnectorWizardPage(this));
+  m_connectorPage->setTitle(tr("Data Source Selection"));
+  m_connectorPage->setSubTitle(tr("Please, select the data source where the data is stored"));
+
+  m_layerSelectionPage.reset(new TL4LayerSelectionWizardPage(this));
+  m_layerSelectionPage->setTitle(tr("Data Source Selection"));
+  m_layerSelectionPage->setSubTitle(tr("Please, select the data source where the data is stored"));
+
+  setPage(0, m_connectorPage.get());
+  setPage(1, m_layerSelectionPage.get());
+
+}
+
+te::qt::plugins::terralib4::TL4ConverterWizard::~TL4ConverterWizard()
+{
+}
+
+int te::qt::plugins::terralib4::TL4ConverterWizard::nextId() const
+{
+  return QWizard::nextId();
+}
+
+void te::qt::plugins::terralib4::TL4ConverterWizard::back()
+{
+
+}
+
+void te::qt::plugins::terralib4::TL4ConverterWizard::next()
+{
+
+}
+
+void te::qt::plugins::terralib4::TL4ConverterWizard::commit()
+{
+
+}
+
+void te::qt::plugins::terralib4::TL4ConverterWizard::help()
+{
+  QMessageBox::warning(this,
+                       tr("TerraLib Qt Components"),
+                       tr("This option is not implemented yet!\nWe will provide it soon!"));
+}
+
+
