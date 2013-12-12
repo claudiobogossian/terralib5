5bfd72389fcebc5c815ffc6bc1303902989e3f05
karine <karine@dpi.inpe.br>
2013-12-12 08:58:20 -0200

    organização dos exemplos de do modulo st

--- examples/st/PrintTimeSeries.cpp
+++ examples/st/PrintTimeSeries.cpp
@@ -10,24 +10,73 @@
 // STL
 #include <iostream>
 
+void PrintTimeSeries(boost::ptr_vector<te::st::TimeSeries>& input)
+{
+  for(std::size_t i=0; i<input.size(); ++i)
+  {
+	  std::cout << "Printing observations of the time series " << i << " :" << std::endl;
+    PrintTimeSeries(&input[i]);
+  }
+}
+
 void PrintTimeSeries(te::st::TimeSeries* ts)
 {
   if(ts == 0)
   {
-    std::cout << "The time series is NULL!" << std::endl;
+    std::cout << "Time series is NULL!" << std::endl;
     return;
   }
 
-  std::cout << "Printing the time series " << std::endl;
-  std::cout << "============================================================" << std::endl;
-    
+  PrintTimeSeriesInfo(ts);
+
+  std::cout << "Time series observations: " << std::endl;
+  
   te::st::TimeSeriesIterator it = ts->begin();  
-    
   while(it != ts->end())
   {
-    std::cout << "Date/Time: " <<  it.getTime()->toString() << std::endl;
+    std::cout << "Date and time: " <<  it.getTime()->toString() << std::endl;
     std::cout << "Value: " <<  it.getValue()->toString()  << std::endl << std::endl;     
     ++it;
   }
 }
+
+void PrintTimeSeriesInfo(boost::ptr_vector<te::st::TimeSeries>& input)
+{
+  for(std::size_t i=0; i<input.size(); ++i)
+  {
+	  std::cout << "Printing information about the time series " << i << " :" << std::endl;
+    PrintTimeSeriesInfo(&input[i]);
+  }
+}
+
+void PrintTimeSeriesInfo(te::st::TimeSeries* ts)
+{
+  if(ts == 0)
+  {
+    std::cout << "Time series is NULL!" << std::endl;
+    return;
+  }
+  
+  std::cout << "Time series id: " << ts->getId() << std::endl;
+  std::cout << "Time series temporal extent: " << ts->getTemporalExtent()->toString() << std::endl;
+
+  double minValue = 0.;
+  double maxValue = 0.;
+  ts->getValueExtent(minValue, maxValue);
+  std::cout << "Time series value extent: (" << minValue << "," << maxValue << ")" << std::endl;
+
+  te::gm::Geometry* gm = ts->getLocation();
+  if(gm!=0)
+    std::cout << "Time series location: " << gm->toString() << std::endl;  
+}
+
+
+
+
+
+
+
+
+
+
 
