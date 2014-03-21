cb08d7e35bd26d1050ef785921dadf0f19d9dc23
Frederico Bede <frederico.bede@funcate.org.br>
2014-03-20 22:21:25 -0300

    Mudanças no script CMake para que o instalador Windows NSIS reinstale todos os arquivos. close #671

--- build/modules/NSISPackage.cmake
+++ build/modules/NSISPackage.cmake
@@ -1,5 +1,3 @@
-include (InstallRequiredSystemLibraries)
-
 # There is a bug in NSI that does not handle full unix paths properly. Make
 # sure there is at least one set of four (4) backlasshes.
 
@@ -12,12 +10,12 @@ set(CPACK_NSIS_URL_INFO_ABOUT "http:\\\\\\\\www.funcate.org.br")
 IF(CMAKE_CL_64) 
   SET(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64") 
   SET(CPACK_NSIS_PACKAGE_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY} (Win64)") 
-  SET(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "${CPACK_PACKAGE_NAME} ${CPACK_PACKAGE_VERSION} (Win64)") 
 ELSE() 
   SET(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES") 
   SET(CPACK_NSIS_PACKAGE_NAME "${CPACK_PACKAGE_INSTALL_DIRECTORY}") 
-  SET(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "${CPACK_PACKAGE_NAME} ${CPACK_PACKAGE_VERSION}") 
 ENDIF() 
+
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY ${CPACK_PACKAGE_NAME}) 
 
 # Creating TERRALIB_DIR enviroment variable.
 list (APPEND CPACK_NSIS_EXTRA_INSTALL_COMMANDS "  WriteRegExpandStr HKLM 'SYSTEM\\\\CurrentControlSet\\\\Control\\\\Session Manager\\\\Environment' 'TERRALIB_DIR' '$INSTDIR'  \\n  SendMessage \\\${HWND_BROADCAST} \\\${WM_WININICHANGE} 0 \\\"STR:Environment\\\" /TIMEOUT=5000")
@@ -36,4 +34,7 @@ list (APPEND CPACK_NSIS_DELETE_ICONS_EXTRA ${CPACK_NSIS_DELETE_ICONS_EXTRA}
 "\nDelete \\\"$DESKTOP\\\\TerraView-5.0.0-alpha.lnk\\\""
 )
 
+include (InstallRequiredSystemLibraries)
+
 INSTALL(FILES ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS} DESTINATION bin COMPONENT BINARIES)
+
