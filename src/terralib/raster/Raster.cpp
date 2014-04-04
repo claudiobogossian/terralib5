08ca24c32f179a6a05a7e6d9e1fa99cbcb55a99c
castejon <castejon@dpi.inpe.br>
2014-04-04 15:00:44 -0300

    Fixing grid UL coord

--- src/terralib/raster/Raster.cpp
+++ src/terralib/raster/Raster.cpp
@@ -516,13 +516,13 @@ te::rst::Raster* te::rst::Raster::resample(int method, unsigned int drow, unsign
   assert(drow + height <= getNumberOfRows());
   assert(dcolumn + width <= getNumberOfColumns());
 
-  te::gm::Coord2D ll = getGrid()->gridToGeo(dcolumn, drow + height);
-  te::gm::Coord2D ur = getGrid()->gridToGeo(dcolumn + width, drow);
-
-  te::gm::Envelope* newbox = new te::gm::Envelope(ll.x, ll.y, ur.x, ur.y);
+  te::gm::Coord2D ulc = getGrid()->gridToGeo(((double)dcolumn) - 0.5, ((double)drow) - 0.5);
+  double newResX = m_grid->getExtent()->getWidth() / ((double)newwidth);
+  double newResY = m_grid->getExtent()->getHeight() / ((double)newheight);  
 
 // create output parameters and raster
-  te::rst::Grid* grid = new te::rst::Grid(newwidth, newheight, newbox, getSRID());
+  te::rst::Grid* grid = new te::rst::Grid(newwidth, newheight, newResX,
+    newResY, &ulc, getSRID());
 
   std::vector<te::rst::BandProperty*> bands;
 
