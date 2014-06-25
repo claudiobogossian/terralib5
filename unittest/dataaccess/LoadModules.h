2c289f1e30193a8a7e3a2447ae9541a36038c6cf
juan <juan@dpi.inpe.br>
2014-06-25 10:21:55 -0300

    Fixed the loading of plugins.

--- unittest/dataaccess/LoadModules.h
+++ unittest/dataaccess/LoadModules.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+/*  Copyright (C) 2008-2014 National Institute For Space Research (INPE) - Brazil.
 
     This file is part of the TerraLib - a Framework for building GIS enabled applications.
 
@@ -33,28 +33,36 @@
 /*!
   \brief Load terralib modules.
  */
+
 void LoadModules()
 {
   try
-  {   
+  {
     te::plugin::PluginInfo* info;
-      
-    info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.ogr.teplg"));
-    te::plugin::PluginManager::getInstance().add(info); 
 
-    info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.gdal.teplg"));
+    std::string plugins_path = te::common::FindInTerraLibPath("share/terralib/plugins");
+
+#ifdef TERRALIB_MOD_OGR_ENABLED
+    info = te::plugin::GetInstalledPlugin(plugins_path + "/te.da.ogr.teplg");
     te::plugin::PluginManager::getInstance().add(info);
-       
-    info = te::plugin::GetInstalledPlugin(TE_PLUGINS_PATH + std::string("/te.da.pgis.teplg"));
-    te::plugin::PluginManager::getInstance().add(info);       
-  
-    te::plugin::PluginManager::getInstance().loadAll(); 
+#endif
+
+#ifdef TERRALIB_MOD_GDAL_ENABLED
+    info = te::plugin::GetInstalledPlugin(plugins_path + "/te.da.gdal.teplg");
+    te::plugin::PluginManager::getInstance().add(info);
+#endif
+
+#ifdef TERRALIB_MOD_POSTGIS_ENABLED
+    info = te::plugin::GetInstalledPlugin(plugins_path + "/te.da.pgis.teplg");
+    te::plugin::PluginManager::getInstance().add(info);
+#endif
+
+  te::plugin::PluginManager::getInstance().loadAll();
   }
   catch(const te::common::Exception& e)
   {
     throw e;
-  }  
-
-};
+  }
+}
 
 #endif  // __TERRALIB_UNITTEST_DATAACCESS_LOADMODULES_H
