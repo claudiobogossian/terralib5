e365fd77e49b715bd4d84ac3f2a60558fae51257
eric.abreu <eric@dpi.inpe.br>
2014-07-18 10:23:28 -0300

    added Spatial Analysis Module and remove unnecessary files from Graph module.

---
+++ src/terralib/sa/core/GPMBuilder.h
@@ -0,0 +1,98 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file GPMBuilder.h
+
+  \brief This class defines the GPM Builder class.
+*/
+
+#ifndef __TERRALIB_SA_INTERNAL_GPMBUILDER_H
+#define __TERRALIB_SA_INTERNAL_GPMBUILDER_H
+
+// Terralib Includes
+#include "../Config.h"
+#include "GeneralizedProximityMatrix.h"
+
+// STL Includes
+#include <map>
+
+namespace te
+{
+  // Forward declarations
+  namespace da { class DataSource; }
+
+  namespace sa
+  {
+    // Forward declarations
+    class GPMConstructorAbstractStrategy;
+  
+    /*!
+      \class GPMBuilder
+
+      \brief This class defines the GPM Builder class.
+
+    */
+
+    class TESAEXPORT GPMBuilder
+    {
+      public:
+
+        /*! \brief Default constructor. */
+        GPMBuilder(GPMConstructorAbstractStrategy* constructor);
+
+        /*! \brief Virtual destructor. */
+        virtual ~GPMBuilder();
+
+        
+        /** @name Methods
+         *  Methods used by the GPM builder
+         */
+        //@{
+
+        /*!
+          \brief Function used to create a empty gpm (using a MEMORY DIRECT graph)
+          
+          \param ds                 Data source pointer
+          \param dataSetName        Container with data source information
+          \param attributeName      Attribute used to define the output graph type
+
+          \return True if the gmp was correctly generated and false in other case.
+
+        */
+        bool setGPMInfo(te::da::DataSourcePtr ds, const std::string& dataSetName, const std::string& attributeName);
+
+        std::auto_ptr<GeneralizedProximityMatrix> build();
+
+        //@}
+
+      private:
+
+        GPMConstructorAbstractStrategy* m_constructor;    //!< Strategy used to construct the gpm.
+
+        std::auto_ptr<GeneralizedProximityMatrix> m_gpm;  //!< GPM reference pointer.
+
+        te::da::DataSourcePtr m_ds;                       //!< Input Data Source.
+
+    };
+
+  } // end namespace sa
+} // end namespace te
+
+#endif //__TERRALIB_SA_INTERNAL_GPMBUILDER_H
---
+++ src/terralib/sa/core/GPMConstructorAbstractStrategy.cpp
@@ -0,0 +1,141 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file GPMConstructorAbstractStrategy.cpp
+
+  \brief This class defines a an Abstract class for a GPM constructor.
+*/
+
+// Terralib Includes
+#include "../../dataaccess/dataset/DataSet.h"
+#include "../../dataaccess/datasource/DataSource.h"
+#include "../../dataaccess/utils/Utils.h"
+#include "../../geometry/GeometryProperty.h"
+#include "../../geometry/MultiPolygon.h"
+#include "../../geometry/Point.h"
+#include "../../geometry/Polygon.h"
+#include "../../graph/core/Vertex.h"
+#include "../../graph/core/AbstractGraph.h"
+#include "../../graph/graphs/DirectedGraph.h"
+#include "GeneralizedProximityMatrix.h"
+#include "GPMConstructorAbstractStrategy.h"
+
+te::sa::GPMConstructorAbstractStrategy::GPMConstructorAbstractStrategy() : m_ds(0), m_gpm(0)
+{
+  m_edgeId = 0;
+}
+
+te::sa::GPMConstructorAbstractStrategy::~GPMConstructorAbstractStrategy()
+{
+}
+
+te::sa::GPMConstructorStrategyType te::sa::GPMConstructorAbstractStrategy::getConstructorType()
+{
+  return m_type;
+}
+
+void te::sa::GPMConstructorAbstractStrategy::construct(te::da::DataSource* ds, GeneralizedProximityMatrix* gpm)
+{
+  assert(ds);
+  m_ds = ds;
+
+  assert(gpm);
+  m_gpm = gpm;
+
+  //build vertex
+  createVertexObjects();
+
+  // build edges using specific strategy
+  constructStrategy();
+}
+
+void te::sa::GPMConstructorAbstractStrategy::createVertexObjects()
+{
+  //get input data information
+  std::auto_ptr<te::da::DataSet> dataSet = m_ds->getDataSet(m_gpm->getDataSetName());
+
+  std::auto_ptr<te::da::DataSetType> dataSetType = m_ds->getDataSetType(m_gpm->getDataSetName());
+
+  te::gm::GeometryProperty* gp = te::da::GetFirstGeomProperty(dataSetType.get());
+
+  int srid = gp->getSRID();
+
+  std::size_t geomPos = te::da::GetFirstSpatialPropertyPos(dataSet.get());
+
+  //create graph vertex attrs
+  te::gm::GeometryProperty* gProp = new te::gm::GeometryProperty(TE_SA_GEOMETRY_ATTR_NAME);
+  gProp->setId(0);
+  gProp->setGeometryType(te::gm::PointType);
+  gProp->setSRID(srid);
+
+  m_gpm->getGraph()->addVertexProperty(gProp);
+
+  //create graph vertex
+  dataSet->moveBeforeFirst();
+
+  while(dataSet->moveNext())
+  {
+    int id = dataSet->getInt32(m_gpm->getAttributeName());
+
+    te::graph::Vertex* v = new te::graph::Vertex(id);
+    
+    v->setAttributeVecSize(1);
+
+    std::auto_ptr<te::gm::Geometry> g = dataSet->getGeometry(geomPos);
+
+    te::dt::AbstractData* ad = 0;
+
+    if(g->getGeomTypeId() == te::gm::PointType)
+    {
+      g->setSRID(srid);
+
+      ad = g->clone();
+    }
+    else if(g->getGeomTypeId() == te::gm::PolygonType)
+    {
+      te::gm::Point* p = ((te::gm::Polygon*)g.get())->getCentroid();
+      p->setSRID(srid);
+
+      ad = p;
+    }
+    else if(g->getGeomTypeId() == te::gm::MultiPolygonType)
+    {
+      te::gm::Polygon* poly = (te::gm::Polygon*)((te::gm::MultiPolygon*)g.get())->getGeometryN(0);
+
+      te::gm::Point* p = poly->getCentroid();
+      p->setSRID(srid);
+
+      ad = p;
+    }
+
+    v->addAttribute(0, ad);
+
+    m_gpm->getGraph()->add(v);
+  }
+}
+
+int te::sa::GPMConstructorAbstractStrategy::getEdgeId()
+{
+  int id = m_edgeId;
+
+  m_edgeId++;
+
+  return id;
+}
