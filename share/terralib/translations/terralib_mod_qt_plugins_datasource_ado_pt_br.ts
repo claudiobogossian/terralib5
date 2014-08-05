e3ac5cfcd0bc808ede74c223bc82f2ae78b30359
Frederico Bede <frederico.bede@funcate.org.br>
2014-08-05 20:11:17 -0300

    Inserting the project and translation files.

---
+++ share/terralib/translations/terralib_mod_qt_plugins_datasource_ado_pt_br.ts
@@ -0,0 +1,220 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>ADOConnectorDialogForm</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="14"/>
+        <source>Microsoft Access Connector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="44"/>
+        <source>Microsoft Access</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="54"/>
+        <source>Base Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="70"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="84"/>
+        <source>Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="109"/>
+        <source>A more detailed description about the data source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="123"/>
+        <source>Connection Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="136"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="149"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="179"/>
+        <source>File/URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="188"/>
+        <source>Provider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="218"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="240"/>
+        <source>&amp;Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="247"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOConnectorDialogForm.ui" line="254"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ADOCreatorDialogForm</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="20"/>
+        <source>Microsoft Access Creator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="47"/>
+        <source>Microsoft Access</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="57"/>
+        <source>Server Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="65"/>
+        <source>Provider</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="90"/>
+        <source>Creation Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="104"/>
+        <source>New Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="116"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="127"/>
+        <source>Create OGC metadata tables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="139"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="179"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="201"/>
+        <source>&amp;Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ui/ADOCreatorDialogForm.ui" line="208"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::ado::ADOConnectorDialog</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="155"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="162"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="193"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="199"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="205"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="213"/>
+        <source>TerraLib Qt Components</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="163"/>
+        <source>Unknown error while opening ADO database!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="194"/>
+        <source>Data source is ok!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="206"/>
+        <source>Unknown error while testing ADO data source!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="214"/>
+        <source>Not implemented yet!
+We will provide it soon!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="219"/>
+        <source>Open ADO Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOConnectorDialog.cpp" line="219"/>
+        <source>Database files (*.accdb *.mdb);; All Files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::ado::ADOCreatorDialog</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="143"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="150"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="161"/>
+        <source>TerraLib Qt Components</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="151"/>
+        <source>Unknown error while opening ADO database!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="162"/>
+        <source>Not implemented yet!
+We will provide it soon!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="167"/>
+        <source>Save ADO Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/ado/ADOCreatorDialog.cpp" line="167"/>
+        <source>Database files (*.accdb *.mdb);; All Files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ share/terralib/translations/terralib_mod_qt_plugins_rp_pt_br.ts
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>te::qt::plugins::rp::ArithmeticOpAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ArithmeticOpAction.cpp" line="40"/>
+        <source>Arithmetic Operations...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::ClassifierAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ClassifierAction.cpp" line="40"/>
+        <source>Classifier...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::ClippingAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ClippingAction.cpp" line="41"/>
+        <location filename="../../../src/terralib/qt/plugins/rp/ClippingAction.cpp" line="43"/>
+        <source>Clipping...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ClippingAction.cpp" line="83"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ClippingAction.cpp" line="83"/>
+        <source>The layer selected is invalid or does not have an raster representation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::ColorTransformAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ColorTransformAction.cpp" line="40"/>
+        <source>Color Transform...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::ContrastAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ContrastAction.cpp" line="40"/>
+        <location filename="../../../src/terralib/qt/plugins/rp/ContrastAction.cpp" line="42"/>
+        <source>Contrast...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ContrastAction.cpp" line="82"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/ContrastAction.cpp" line="82"/>
+        <source>The layer selected is invalid or does not have an raster representation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::FilterAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/FilterAction.cpp" line="40"/>
+        <source>Filter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::FusionAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/FusionAction.cpp" line="40"/>
+        <source>Fusion...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::MixtureModelAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/MixtureModelAction.cpp" line="40"/>
+        <location filename="../../../src/terralib/qt/plugins/rp/MixtureModelAction.cpp" line="42"/>
+        <source>Mixture Model...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/MixtureModelAction.cpp" line="82"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/MixtureModelAction.cpp" line="82"/>
+        <source>The layer selected is invalid or does not have an raster representation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::MosaicAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/MosaicAction.cpp" line="40"/>
+        <source>Mosaic...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::RegisterAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/RegisterAction.cpp" line="39"/>
+        <source>Register...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::SegmenterAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/SegmenterAction.cpp" line="41"/>
+        <location filename="../../../src/terralib/qt/plugins/rp/SegmenterAction.cpp" line="43"/>
+        <source>Segmenter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/SegmenterAction.cpp" line="83"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/SegmenterAction.cpp" line="83"/>
+        <source>The layer selected is invalid or does not have an raster representation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::rp::VectorizationAction</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/rp/VectorizationAction.cpp" line="29"/>
+        <location filename="../../../src/terralib/qt/plugins/rp/VectorizationAction.cpp" line="30"/>
+        <source>Vectorization...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
