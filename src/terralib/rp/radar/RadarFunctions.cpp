794afffbf30d561d8547e199f9ed0caf5310db26
castejon <castejon@dpi.inpe.br>
2014-11-03 15:25:08 -0200

    Openning room for radar raster processing

---
+++ src/terralib/rp/radar/RadarFunctions.cpp
@@ -0,0 +1,41 @@
+/*  Copyright (C) 2001-2009 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/rp/radar/RadarFunctions.cpp
+
+  \brief Radar Raster Processing functions.
+*/
+
+#include "RadarFunctions.h"
+
+namespace te
+{
+  namespace rp
+  {
+    namespace radar
+    {
+      bool SampleFunction( const te::rst::Raster& inputRaster )
+      {
+        return false;
+      }
+
+    } // end namespace radar
+  } // end namespace rp
+}   // end namespace te
