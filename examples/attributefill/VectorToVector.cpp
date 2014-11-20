87c5f37fc0a758c18c9c2b3094e956ba60b277f4
Ruan <ruan.andrade@funcate.org.br>
2014-11-20 16:19:39 -0200

    refs #833

---
+++ examples/attributefill/VectorToVector.cpp
@@ -0,0 +1,25 @@
+
+#include "../Config.h"
+#include <terralib/common.h>
+#include <terralib/dataaccess.h>
+#include <terralib/dataaccess/datasource/DataSourceFactory.h>
+#include <terralib/attributefill/VectorToVectorOp.h>
+#include <terralib/attributefill/VectorToVectorMemory.h>
+
+// STL
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+
+bool VectorToVectorInSHP()
+{
+  return false;
+}
+
+bool VectorToVectorInPGIS()
+{
+  return false;
+}
