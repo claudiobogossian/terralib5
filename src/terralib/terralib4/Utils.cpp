7ee1bb064b2e355ef3367913e371bc048a334f23
felipe.siqueira <felipe.siqueira@funcate.org.br>
2014-12-08 16:57:00 -0200

    Solved bug with numeric

--- src/terralib/terralib4/Utils.cpp
+++ src/terralib/terralib4/Utils.cpp
@@ -146,7 +146,7 @@ int terralib4::Convert2T5(TeAttrDataType type)
       return te::dt::STRING_TYPE;
 
     case TeREAL:
-      return te::dt::NUMERIC_TYPE;
+      return te::dt::DOUBLE;
 
     case TeINT:
       return te::dt::INT32_TYPE;
