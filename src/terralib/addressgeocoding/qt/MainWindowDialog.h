7a69cd2d0ae6e9d923b7022caa48540545ef749e
Ruan <ruan.andrade@funcate.org.br>
2015-02-04 14:10:51 -0200

    refs #255

--- src/terralib/addressgeocoding/qt/MainWindowDialog.h
+++ src/terralib/addressgeocoding/qt/MainWindowDialog.h
@@ -88,9 +88,13 @@ namespace te
 
         void onConfigureLayerClicked();
 
+        void onAddressComboBoxChanged(int index);
+
         void onTargetFileAddressToolButtonPressed();
 
-        void onConfigureAddressClicked();
+        void onEditAddressToolButtonPressed();
+
+        void onRemoveAddressToolButtonPressed();
 
         void onTargetDatasourceToolButtonPressed();
 
@@ -103,6 +107,17 @@ namespace te
         void onCancelPushButtonClicked();
 
       private:
+
+        void GetAddressFilePathToSettings(std::string& filePath);
+
+        void GetAddressConfigToSettings(std::string& streetType,
+                                        std::string& streetTitle,
+                                        std::string& streetName,
+                                        std::string& number,
+                                        std::string& neighborhood,
+                                        std::string& postalCode);
+
+        void GetDataSourceAddress(std::string fileName);
 
         std::auto_ptr<Ui::MainWindowDialogForm> m_ui;
         std::list<te::map::AbstractLayerPtr> m_layers;                //!< List of layers.
