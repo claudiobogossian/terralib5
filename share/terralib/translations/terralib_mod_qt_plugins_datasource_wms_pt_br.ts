7b7a89c0b4e75b90f2c9dee9ea9bb1fffeb1fe51
Frederico Bedê <frederico.bede@funcate.org.br>
2015-04-24 11:11:27 -0300

    Arquivos de tradução e asserto do projeto de traduções.

---
+++ share/terralib/translations/terralib_mod_qt_plugins_datasource_wms_pt_br.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>WMSConnectorDialogForm</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="14"/>
+        <source>WMS Connector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="39"/>
+        <source>WMS - Web Map Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="51"/>
+        <source>Service Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="70"/>
+        <source>Data Source Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="80"/>
+        <source>Data Source Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="99"/>
+        <source>A more detailed description about the data source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="115"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="137"/>
+        <source>&amp;Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="144"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/ui/WMSConnectorDialogForm.ui" line="151"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::wms::WMSConnector</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnector.cpp" line="74"/>
+        <source>TerraLib Qt Components</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnector.cpp" line="74"/>
+        <source>The WMS data access driver not support creation operation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>te::qt::plugins::wms::WMSConnectorDialog</name>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="145"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="151"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="180"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="188"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="194"/>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="202"/>
+        <source>TerraLib Qt Components</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="152"/>
+        <source>Unknown error while opening WMS data source!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="181"/>
+        <source>Data source is ok!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="195"/>
+        <source>Unknown error while testing WMS data source!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/terralib/qt/plugins/datasource/wms/WMSConnectorDialog.cpp" line="203"/>
+        <source>Not implemented yet!
+We will provide it soon!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
