823bab02110900e31c86a3fa23f2c4d2f54bfe7f
andre.oliveira <andre.oliveira@funcate.org.br>
2015-05-08 15:14:23 -0300

    Updating the documentation related to TimeSeries data and missing include files.

--- src/terralib/st/loader/STDataLoaderFromDS.cpp
+++ src/terralib/st/loader/STDataLoaderFromDS.cpp
@@ -302,7 +302,7 @@ te::st::STDataLoaderFromDS::getDataSet(const TimeSeriesDataSetInfo& info,
   //get the data set applying he filter
   std::auto_ptr<te::da::DataSet> dset(ds->getDataSet(info.getObservationDataSetInfo().getDataSetName(), geomPropName, &geom, r, travType));
 
-  return buildDataSet(dset.release(), info); 
+  return buildDataSet(dset.release(), info);
 }
 
 std::auto_ptr<te::st::TimeSeriesDataSet>
@@ -323,7 +323,7 @@ te::st::STDataLoaderFromDS::getDataSet(const TimeSeriesDataSetInfo& info,
   std::auto_ptr<te::da::DataSet> dset(ds->getDataSet(info.getObservationDataSetInfo().getDataSetName(), 
                           geomPropName, &e, r, travType));
 
-  return buildDataSet(dset.release(), info);  
+  return buildDataSet(dset.release(), info);
 }
 
 std::auto_ptr<te::st::TimeSeriesDataSet> 
