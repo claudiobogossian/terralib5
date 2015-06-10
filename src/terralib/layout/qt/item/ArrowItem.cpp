002e06651fadc2922bd91c07f91950bed25ff513
juliana.coelho <juliana.coelho@funcate.org.br>
2015-06-10 09:33:10 -0300

    Arrow correction ArrowItem.cpp

--- src/terralib/layout/qt/item/ArrowItem.cpp
+++ src/terralib/layout/qt/item/ArrowItem.cpp
@@ -95,21 +95,22 @@ void te::layout::ArrowItem::drawRightArrow( QPainter * painter )
 	painter->save();
 
 	double w1 = boundingRect().width() / 2.;
-	double w3 = boundingRect().width() / 40.;
+	double w2 = boundingRect().width() / 100.;
 	double h1 = boundingRect().height() / 2.;
 	double h2 = boundingRect().height() / 4.;
 	double y1 = boundingRect().center().y()-h1;
 	double y2 = boundingRect().center().y()+h1;
 	double y3 = boundingRect().center().y()-h2;
 	double y4 = boundingRect().center().y()+h2;
+	double y5 = boundingRect().width() - w2;
 
 	QPointF p1 = QPointF(w1,y2);
-	QPointF p2 = QPointF(y2,w1);
+	QPointF p2 = QPointF(y5,h1);
 	QPointF p3 = QPointF(w1,y1);
-	QPointF p4 = QPointF(h1,y3);
-	QPointF p5 = QPointF(w3,y3);
-	QPointF p6 = QPointF(w3,y4);
-	QPointF p7 = QPointF(h1,y4);
+	QPointF p4 = QPointF(w1,y3);
+	QPointF p5 = QPointF(w2,y3);
+	QPointF p6 = QPointF(w2,y4);
+	QPointF p7 = QPointF(w1,y4);
 	QPolygonF arrowPolygon;
 	arrowPolygon<<p1<<p2<<p3<<p4<<p5<<p6<<p7;
 	painter->drawPolygon(arrowPolygon);
@@ -118,24 +119,28 @@ void te::layout::ArrowItem::drawRightArrow( QPainter * painter )
 
 void te::layout::ArrowItem::drawDoubleArrow(QPainter * painter)
 {
-	double w1 = boundingRect().width() / 2.;
+
 	double w2 = boundingRect().width() / 4.;
 	double w3 = boundingRect().width() - boundingRect().width() / 4.;
+	double w4 = boundingRect().width() / 100.;
 	double h1 = boundingRect().height() / 2.;
 	double h2 = boundingRect().height() / 4.;
 	double y1 = boundingRect().center().y()-h1;
 	double y2 = boundingRect().center().y()+h1;
 	double y4 = boundingRect().center().y()+h2;
+	double y5 = boundingRect().width() - w4;
+	double y6 = boundingRect().center().y() + w4;
+
 	painter->save();
 	QPointF p1 = QPointF(w3,y2);
-	QPointF p2 = QPointF(y2,w1);
+	QPointF p2 = QPointF(y5,h1);
 	QPointF p3 = QPointF(w3,y1);
 	QPointF p4 = QPointF(w3,h2);
-	QPointF p5 = QPointF(h2,w2);
+	QPointF p5 = QPointF(w2,h2); 
 	QPointF p6 = QPointF(w2,y1);
-	QPointF p7 = QPointF(y1,w1);
+	QPointF p7 = QPointF(w4,y6);
 	QPointF p8 = QPointF(w2,y2);
-	QPointF p9 = QPointF(h2,y4);
+	QPointF p9 = QPointF(w2,y4);
 	QPointF p10 = QPointF(w3,y4);
 	QPolygonF arrowPolygon;
 	arrowPolygon<<p1<<p2<<p3<<p4<<p5<<p6<<p7<<p8<<p9<<p10;
