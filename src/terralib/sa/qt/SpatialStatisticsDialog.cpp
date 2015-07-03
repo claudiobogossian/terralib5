40a333414864585f58c61994841802f7727067f3
Lubia Vinhas <lubia@dhcp50.dpi.inpe.br>
2015-07-03 15:02:17 -0300

    removing unused variables

--- src/terralib/sa/qt/SpatialStatisticsDialog.cpp
+++ src/terralib/sa/qt/SpatialStatisticsDialog.cpp
@@ -395,7 +395,7 @@ void te::sa::SpatialStatisticsDialog::onOkPushButtonClicked()
 
   //associate the input geometry attribute to the GPM
   te::gm::GeometryProperty* gmProp = te::da::GetFirstGeomProperty(dataSetType.get());
-  int geomIdx = te::sa::AssociateGPMVertexAttribute(gpm.get(), ds.get(), dsLayer->getDataSetName(), attrLink, gmProp->getName(), gmProp->getType(), gmProp->getSRID(), gmProp->getGeometryType());
+  te::sa::AssociateGPMVertexAttribute(gpm.get(), ds.get(), dsLayer->getDataSetName(), attrLink, gmProp->getName(), gmProp->getType(), gmProp->getSRID(), gmProp->getGeometryType());
 
   try
   {
