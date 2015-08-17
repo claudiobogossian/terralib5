4d7c3091eda5496348c0944cedad6550c662b159
gribeiro <gribeiro@dpi.inpe.br>
2015-08-17 16:40:44 -0300

    removing warnings...

--- unittest/dataaccess/main.cpp
+++ unittest/dataaccess/main.cpp
@@ -52,7 +52,7 @@ int main(int /*argc*/, char** /*argv*/)
   LoadModules();
 
   std::string report_dir = TERRALIB_REPORT_DIR;
-  bool resultStatus;
+  bool resultStatus = false;
  
   boost::property_tree::ptree drivers;
 
