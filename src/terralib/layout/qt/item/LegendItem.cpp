0d634dd8a6741c92c842137e58fe8633370a73b7
Mário Rocco Pettinati <mario@funcate.org.br>
2015-08-21 14:44:28 -0300

    Adapting LegendItem to the new MVM Item Model

--- src/terralib/layout/qt/item/LegendItem.cpp
+++ src/terralib/layout/qt/item/LegendItem.cpp
@@ -28,7 +28,7 @@
 // TerraLib
 #include "LegendItem.h"
 #include "..\..\core\pattern\mvc\AbstractItemController.h"
-//#include "../../core/pattern/singleton/Context.h"
+#include "../../core/pattern/singleton/Context.h"
 //#include "../../core/pattern/mvc/ItemController.h"
 //#include "../core/Scene.h"
 //#include "../../core/pattern/mvc/Observable.h"
@@ -41,14 +41,14 @@
 //#include "../../../common/STLUtils.h"
 //#include "../../item/LegendModel.h"
 //#include "../../../maptools/AbstractLayer.h"
-//#include "../../../maptools/GroupingItem.h"
-//#include "../../../maptools/Grouping.h"
-//#include "../../../maptools/Canvas.h"
-//#include "../../../maptools/CanvasConfigurer.h"
-//#include "../../../qt/widgets/canvas/Canvas.h"
-//#include "../../../se/Symbolizer.h"
-//#include "../../../se/PolygonSymbolizer.h"
-//#include "../../../se/Fill.h"
+#include "../../../maptools/GroupingItem.h"
+#include "../../../maptools/Grouping.h"
+#include "../../../maptools/Canvas.h"
+#include "../../../maptools/CanvasConfigurer.h"
+#include "../../../qt/widgets/canvas/Canvas.h"
+#include "../../../se/Symbolizer.h"
+#include "../../../se/PolygonSymbolizer.h"
+#include "../../../se/Fill.h"
 //#include "../../../se/Utils.h"
 //#include "../../../color/RGBAColor.h"
 
@@ -62,19 +62,19 @@
 #include <QColor>
 #include <QMatrix>
 
-te::layout::LegendItem::LegendItem(AbstractItemController* controller, AbstractItemModel* model, bool invertedMatrix ) 
-  : AbstractItem<QGraphicsItem>(controller, model)
+te::layout::LegendItem::LegendItem(AbstractItemController* controller, AbstractItemModel* model )
+  : AbstractItem<QGraphicsItem>(controller, model, true)
   //m_move(false)
 {  
-  //this->setFlags(QGraphicsItem::ItemIsMovable
-  //  | QGraphicsItem::ItemIsSelectable
-  //  | QGraphicsItem::ItemSendsGeometryChanges
-  //  | QGraphicsItem::ItemIsFocusable);
+  this->setFlags(QGraphicsItem::ItemIsMovable
+    | QGraphicsItem::ItemIsSelectable
+    | QGraphicsItem::ItemSendsGeometryChanges
+    | QGraphicsItem::ItemIsFocusable);
 
   //m_nameClass = std::string(this->metaObject()->className());
 
-  ////The text size or length that exceeds the sides will be cut
-  //setFlag(QGraphicsItem::ItemClipsToShape);
+  //The text size or length that exceeds the sides will be cut
+  setFlag(QGraphicsItem::ItemClipsToShape);
 }
 
 te::layout::LegendItem::~LegendItem()
@@ -109,35 +109,28 @@ te::layout::LegendItem::~LegendItem()
 
 void te::layout::LegendItem::drawItem( QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget )
 {
-  //LegendModel* legendModel = dynamic_cast<LegendModel*> (m_model);
+  te::layout::Utils* utils = Context::getInstance().getUtils(); 
+  te::map::Canvas* canvas = Context::getInstance().getCanvas();
 
-  //te::layout::Utils* utils = Context::getInstance().getUtils(); 
-  //te::map::Canvas* canvas = Context::getInstance().getCanvas();
+  const Property& pFont = m_controller->getProperty("font");
+  const Property& pFontColor = m_controller->getProperty("font_color");
+  const Property& pLayers = m_controller->getProperty("layers");
 
-  //if (legendModel == 0)
-  //{
-  //  return;
-  //}
+  const Font& font = pFont.getValue().toFont();
+  const te::color::RGBAColor& fontColor = pFontColor.getValue().toColor();
+  const std::list<te::map::AbstractLayerPtr>& layerList = pLayers.getValue().toGenericVariant().toLayerList();
 
-  //te::map::AbstractLayerPtr layer = legendModel->getLayer();
+  double borderDisplacement(1);
+  double displacementBetweenTitleAndSymbols(7);
+  double displacementBetweenSymbols(7);
+  double displacementBetweenSymbolsAndText(2);
+  double symbolsize(5);
 
-  //if(!layer)
-  //{
-  //  return;
-  //}
-
-  //std::string title = layer->getTitle();
-
-  //QString qTitle (title.c_str());
-
-  //QRectF boundRect = this->boundingRect();
-  //QMatrix matrix = painter->matrix();
-
-  //te::layout::Font font = legendModel->getFont(); 
-  //te::color::RGBAColor fontColor = legendModel->getFontColor();
-
-  //QFont qfont (QString(font.getFamily().c_str()), font.getPointSize());
-  //QColor qFontColor (fontColor.getRed(), fontColor.getGreen(), fontColor.getBlue(), fontColor.getAlpha());
+  int borderDisplacementInPixels = utils->mm2pixel(borderDisplacement);
+  int dispBetweenSymbolAndTextInPixels = utils->mm2pixel(displacementBetweenSymbolsAndText);
+  int dispBetweenSymbolsInPixels = utils->mm2pixel(displacementBetweenSymbols);
+  int dispBetweenTitleAndSymbolsInPixels = utils->mm2pixel(displacementBetweenTitleAndSymbols);
+  int symbolSizeInPixels = utils->mm2pixel(symbolsize);
 
   //int borderDisplacementInPixels = utils->mm2pixel(legendModel->getBorderDisplacement());
   //int dispBetweenSymbolAndTextInPixels = utils->mm2pixel(legendModel->getDisplacementBetweenSymbolAndText());
@@ -145,128 +138,146 @@ void te::layout::LegendItem::drawItem( QPainter * painter, const QStyleOptionGra
   //int dispBetweenTitleAndSymbolsInPixels = utils->mm2pixel(legendModel->getDisplacementBetweenTitleAndSymbols());
   //int symbolSizeInPixels = utils->mm2pixel(legendModel->getSymbolSize());
 
-  //double x1 = boundRect.x() + borderDisplacementInPixels;
-  //double y1 = boundRect.y() - borderDisplacementInPixels;
 
-  //canvas->setTextPointSize(font.getPointSize());
-  //canvas->setTextUnderline(font.isUnderline());
-  //canvas->setTextStrikeOut(font.isStrikeout());
-  //canvas->setTextColor(legendModel->getFontColor());
+  if(layerList.empty() == true)
+  {
+    return;
+  }
 
-  //double wtxtInPixels = 0.;
-  //double htxtInPixels = 0.;
+  const te::map::AbstractLayerPtr& layer = *layerList.begin();
 
-  //utils->textBoundingBox(wtxtInPixels, htxtInPixels, title);
-  //
-  //QRectF rectTitle (x1, y1, wtxtInPixels, htxtInPixels);
+  std::string title = layer->getTitle();
 
-  //painter->setFont(qfont);
-  //painter->setBrush(qFontColor);
+  QString qTitle (title.c_str());
 
-  //painter->drawText(rectTitle, qTitle);
+  QRectF boundRect = this->boundingRect();
+  QMatrix matrix = painter->matrix();
 
-  //y1 += dispBetweenTitleAndSymbolsInPixels;
+  QFont qfont (QString(font.getFamily().c_str()), font.getPointSize());
+  QColor qFontColor (fontColor.getRed(), fontColor.getGreen(), fontColor.getBlue(), fontColor.getAlpha());
 
-  //double widthBox = 0.;
-  //double heightBox = 0.;
+  double x1 = boundRect.x() + borderDisplacementInPixels;
+  double y1 = boundRect.y() - borderDisplacementInPixels;
 
-  //te::map::Grouping* grouping = layer->getGrouping();
+  canvas->setTextPointSize(font.getPointSize());
+  canvas->setTextUnderline(font.isUnderline());
+  canvas->setTextStrikeOut(font.isStrikeout());
+  canvas->setTextColor(fontColor);
 
-  //if (grouping != 0 && grouping->isVisible() == true)
-  //{
-  //  std::string propertyName = grouping->getPropertyName();
+  double wtxtInPixels = 0.;
+  double htxtInPixels = 0.;
 
-  //  std::vector<te::map::GroupingItem*> items = grouping->getGroupingItems();
+  utils->textBoundingBox(wtxtInPixels, htxtInPixels, title);
+  
+  QRectF rectTitle (x1, y1, wtxtInPixels, htxtInPixels);
 
-  //  te::map::GroupingType type = grouping->getType();
+  painter->setFont(qfont);
+  painter->setBrush(qFontColor);
 
-  //  double labelX1 = x1 + symbolSizeInPixels + dispBetweenSymbolAndTextInPixels;
+  painter->drawText(rectTitle, qTitle);
 
-  //  for (unsigned int i = 0; i < items.size(); ++i)
-  //  {
-  //    std::string label = propertyName;
-  //    label += ": ";
+  y1 += dispBetweenTitleAndSymbolsInPixels;
 
-  //    te::map::GroupingItem* item = items[i];
+  double widthBox = 0.;
+  double heightBox = 0.;
 
-  //    if (type == te::map::UNIQUE_VALUE)
-  //    {
-  //      label += item->getValue();
-  //    }
-  //    else
-  //    {
-  //      std::string upperLimit = item->getUpperLimit();
-  //      std::string lowerLimit = item->getLowerLimit();
+  te::map::Grouping* grouping = layer->getGrouping();
 
-  //      label += lowerLimit;
-  //      label += " ~ ";
-  //      label += upperLimit;
-  //    }
-  //    painter->save();
-  //    painter->setFont(qfont);
-  //    painter->setBrush(qFontColor);
+  if (grouping != 0 && grouping->isVisible() == true)
+  {
+    std::string propertyName = grouping->getPropertyName();
 
-  //    utils->textBoundingBox(wtxtInPixels, htxtInPixels, label);
+    const std::vector<te::map::GroupingItem*>& items = grouping->getGroupingItems();
 
-  //    QRectF labelRect (labelX1, y1, wtxtInPixels, htxtInPixels);
-  //    QString qLabel (label.c_str());
-  //    painter->drawText(labelRect, qLabel);
+    te::map::GroupingType type = grouping->getType();
 
-  //    painter->restore();
+    double labelX1 = x1 + symbolSizeInPixels + dispBetweenSymbolAndTextInPixels;
 
-  //    const std::vector<te::se::Symbolizer*>& symbolizers = item->getSymbolizers();
+    for (unsigned int i = 0; i < items.size(); ++i)
+    {
+      std::string label = propertyName;
+      label += ": ";
 
-  //    te::qt::widgets::Canvas geomCanvas (painter->device());
-  //    geomCanvas.setMatrix(matrix);
+      te::map::GroupingItem* item = items[i];
 
-  //    foreach (te::se::Symbolizer* symbol, symbolizers)
-  //    {
-  //      double offset = 2.0;
-  //      QRectF geomRect (x1, y1, symbolSizeInPixels, symbolSizeInPixels);
-  //      
-  //      te::gm::Geometry* geom = 0;
-  //      if (symbol->getType() == "PolygonSymbolizer")
-  //      {
-  //        te::gm::Polygon* polygon = new te::gm::Polygon(1, te::gm::PolygonType);
-  //        te::gm::LinearRing* ring = new te::gm::LinearRing(5, te::gm::LineStringType);
-  //        ring->setPoint(0, x1 + offset, y1 + offset);
-  //        ring->setPoint(1, x1 + geomRect.width() - offset, y1 + offset);
-  //        ring->setPoint(2, x1 + geomRect.width() - offset, y1 + geomRect.height() - offset);
-  //        ring->setPoint(3, x1 + offset, y1 + geomRect.height() - offset);
-  //        ring->setPoint(4, x1 + offset, y1 + offset);
-  //        polygon->setRingN(0, ring);
-  //        geom = polygon;
-  //      }
-  //      else if (symbol->getType() == "LineSymbolizer")
-  //      {
-  //        te::gm::LineString* line = new te::gm::LineString(2, te::gm::LineStringType);
-  //        line->setPoint(0, x1 + offset, y1 + geomRect.height() * 0.5);
-  //        line->setPoint(1, x1 + geomRect.width() - offset, y1 + geomRect.height() * 0.5);
-  //        geom = line;
-  //      }
-  //      else if (symbol->getType() == "PointSymbolizer")
-  //      {
-  //        geom = new te::gm::Point( x1 +geomRect.width() * 0.5, y1 +geomRect.height() * 0.5);
-  //      }
+      if (type == te::map::UNIQUE_VALUE)
+      {
+        label += item->getValue();
+      }
+      else
+      {
+        const std::string& upperLimit = item->getUpperLimit();
+        const std::string& lowerLimit = item->getLowerLimit();
 
-  //      // Configuring...
-  //      te::map::CanvasConfigurer cc(&geomCanvas);
-  //      cc.config(symbol);
-  //      
-  //      // Let's draw!
-  //      geomCanvas.draw(geom);
+        label += lowerLimit;
+        label += " ~ ";
+        label += upperLimit;
+      }
+      painter->save();
+      painter->setFont(qfont);
+      painter->setBrush(qFontColor);
 
-  //      if(widthBox < (labelRect.width() + symbolSizeInPixels))
-  //      {
-  //        widthBox = labelRect.width() + symbolSizeInPixels;
-  //      }
-  //      
-  //      heightBox+= symbolSizeInPixels + dispBetweenSymbolsInPixels;
-  //    }
+      utils->textBoundingBox(wtxtInPixels, htxtInPixels, label);
 
-  //    y1 += dispBetweenSymbolsInPixels;
-  //  }
-  //}
+      QRectF labelRect (labelX1, y1, wtxtInPixels, htxtInPixels);
+      QString qLabel (label.c_str());
+      painter->drawText(labelRect, qLabel);
+
+      painter->restore();
+
+      const std::vector<te::se::Symbolizer*>& symbolizers = item->getSymbolizers();
+
+      te::qt::widgets::Canvas geomCanvas (painter->device());
+      geomCanvas.setMatrix(matrix);
+
+      foreach (te::se::Symbolizer* symbol, symbolizers)
+      {
+        double offset = 2.0;
+        QRectF geomRect (x1, y1, symbolSizeInPixels, symbolSizeInPixels);
+        
+        te::gm::Geometry* geom = 0;
+        if (symbol->getType() == "PolygonSymbolizer")
+        {
+          te::gm::Polygon* polygon = new te::gm::Polygon(1, te::gm::PolygonType);
+          te::gm::LinearRing* ring = new te::gm::LinearRing(5, te::gm::LineStringType);
+          ring->setPoint(0, x1 + offset, y1 + offset);
+          ring->setPoint(1, x1 + geomRect.width() - offset, y1 + offset);
+          ring->setPoint(2, x1 + geomRect.width() - offset, y1 + geomRect.height() - offset);
+          ring->setPoint(3, x1 + offset, y1 + geomRect.height() - offset);
+          ring->setPoint(4, x1 + offset, y1 + offset);
+          polygon->setRingN(0, ring);
+          geom = polygon;
+        }
+        else if (symbol->getType() == "LineSymbolizer")
+        {
+          te::gm::LineString* line = new te::gm::LineString(2, te::gm::LineStringType);
+          line->setPoint(0, x1 + offset, y1 + geomRect.height() * 0.5);
+          line->setPoint(1, x1 + geomRect.width() - offset, y1 + geomRect.height() * 0.5);
+          geom = line;
+        }
+        else if (symbol->getType() == "PointSymbolizer")
+        {
+          geom = new te::gm::Point( x1 +geomRect.width() * 0.5, y1 +geomRect.height() * 0.5);
+        }
+
+        // Configuring...
+        te::map::CanvasConfigurer cc(&geomCanvas);
+        cc.config(symbol);
+        
+        // Let's draw!
+        geomCanvas.draw(geom);
+
+        if(widthBox < (labelRect.width() + symbolSizeInPixels))
+        {
+          widthBox = labelRect.width() + symbolSizeInPixels;
+        }
+        
+        heightBox+= symbolSizeInPixels + dispBetweenSymbolsInPixels;
+      }
+
+      y1 += dispBetweenSymbolsInPixels;
+    }
+  }
 
   //this->setRect(QRectF(0, 0, widthBox, heightBox));
 }
