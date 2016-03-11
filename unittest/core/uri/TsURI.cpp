b4adba6750c7df2c3b1a2bccff845823baf2f096
vinicius <vinicius.campanha@funcate.org.br>
2016-03-11 11:53:27 -0300

    URI examples and unittest structures

---
+++ unittest/core/uri/TsURI.cpp
@@ -0,0 +1,28 @@
+/*  Copyright (C) 2008 National Institute For Space Research (INPE) - Brazil.
+
+    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+
+    TerraLib is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    TerraLib is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TerraLib. See COPYING. If not, write to
+    TerraLib Team at <terralib-team@terralib.org>.
+ */
+
+/*!
+  \file terralib/unittest/core/uri/TsURI.cpp
+
+  \brief A test suit for the Core URI API.
+
+  \author Vinicius campanha
+ */
+
+#include "TsURI.h"
