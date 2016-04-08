93089c7bb3597c6cc660b703ae4803465e7a2371
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2016-04-08 11:27:53 -0300

    code review: usage of CharEncoding

--- src/terralib/ado/DataSource.cpp
+++ src/terralib/ado/DataSource.cpp
@@ -222,9 +222,9 @@ std::vector<std::string> te::ado::DataSource::getDataSourceNames(const std::map<
   return std::vector<std::string>(); // The DataSource is a File.
 }
 
-std::vector<te::common::CharEncoding> te::ado::DataSource::getEncodings(const std::map<std::string, std::string>&)
+std::vector<te::core::EncodingType> te::ado::DataSource::getEncodings(const std::map<std::string, std::string>&)
 {
-  return std::vector<te::common::CharEncoding>(); //TODO how?
+  return std::vector<te::core::EncodingType>(); //TODO how?
 }
 
 void te::ado::DataSource::loadGeometryColumnsCache(_ConnectionPtr& adoConn)
