cde720ab83851c259e3a4981c7ec31bd783836a5
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2016-05-17 16:37:45 -0300

    Adding functions to core/Logger

--- examples/core/logger/main.cpp
+++ examples/core/logger/main.cpp
@@ -27,14 +27,16 @@
 */
 
 // TerraLib
-
-
-// Boost
-#include <boost/log/trivial.hpp>
+#include <terralib/core/logger/Logger.h>
 
 // STL
 
 int main(int argc, char *argv[])
 {
-// BOOST_LOG_TRIVIAL(fatal) << "Fatal error";
+  TE_LOG_INIT("terralib_example_core_logger");
+  TE_LOG_TRACE("mensagem");
+  {
+    TE_LOG_INIT("segundo_logger");
+    TE_LOG_TRACE("log2");
+  }
 }
