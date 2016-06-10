cf9bea5a559cff2cc9c527188b20c8f1c127a15c
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2016-06-10 10:15:32 -0300

    Updating Doxygen and adding support to load configuration file

--- src/terralib/core/logger/Logger.cpp
+++ src/terralib/core/logger/Logger.cpp
@@ -1,49 +1,44 @@
-/*  Copyright (C) 2008 National Institute For Space Research (INPE) - Brazil.
+/*
+  Copyright (C) 2008 National Institute For Space Research (INPE) - Brazil.
 
-    This file is part of the TerraLib - a Framework for building GIS enabled applications.
+  This file is part of the TerraLib - a Framework for building GIS enabled applications.
 
-    TerraLib is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License,
-    or (at your option) any later version.
+  TerraLib is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
 
-    TerraLib is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU Lesser General Public License for more details.
+  TerraLib is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with TerraLib. See COPYING. If not, write to
-    TerraLib Team at <terralib-team@terralib.org>.
+  You should have received a copy of the GNU Lesser General Public License
+  along with TerraLib. See COPYING. If not, write to
+  TerraLib Team at <terralib-team@terralib.org>.
  */
 
 /*!
-  \file terralib/core/translator/Translator.cpp
+  \file terralib/core/logger/Logger.cpp
 
-  \brief This class is designed for dealing with multi-language text translation in TerraLib.
+  \brief This class is designed to manage the log of information in TerraLib.
 
   \author Matheus Cavassan Zaglia
   \author Gilberto Ribeiro de Queiroz
  */
 
-
 // TerraLib
-#include "../utils/Platform.h"
 #include "Logger.h"
 
 // Boost
-#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/filesystem/fstream.hpp>
 #include <boost/log/trivial.hpp>
-#include <boost/log/expressions/formatters/date_time.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sources/channel_feature.hpp>
-#include <boost/log/sources/channel_logger.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/from_stream.hpp>
 
-BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+typedef boost::log::sinks::synchronous_sink< boost::log::sinks::text_file_backend > text_sink;
 
 te::core::Logger& te::core::Logger::getInstance()
 {
@@ -52,37 +47,32 @@ te::core::Logger& te::core::Logger::getInstance()
   return instance;
 }
 
-void te::core::Logger::SetupLogger(const std::string& filename, const std::string& path)
+void te::core::Logger::setupFromFile(const std::string& filename)
+{
+  boost::log::core::get()->remove_all_sinks();
+  boost::filesystem::ifstream settings(filename);
+  boost::log::init_from_stream(settings);
+}
+
+void te::core::Logger::setupLogger(const std::string& filename)
 {
   m_filename = filename;
-  m_path = path;
 
   boost::log::core::get()->remove_all_sinks();
 
-  boost::log::formatter formatter =
-                           boost::log::expressions::stream
-                        << boost::log::expressions::format_date_time(timestamp, "[%Y-%m-%d %H:%M:%S]") << " <"
-                        << boost::log::trivial::severity << "> : "
-                        << boost::log::expressions::smessage;
-
-  boost::shared_ptr<boost::log::sinks::synchronous_sink< boost::log::sinks::text_file_backend > >
-     sink = boost::log::add_file_log(boost::log::keywords::auto_flush = true,
-                                     boost::log::keywords::file_name = m_filename + "%2N.log",
-                                     boost::log::keywords::target = m_path,
-                                     boost::log::keywords::max_size = 10 * 1024 * 1024,
-                                     boost::log::keywords::rotation_size = 10 * 1024 * 1024,
-                                     boost::log::keywords::scan_method = boost::log::sinks::file::scan_matching,
-                                     boost::log::keywords::open_mode = std::ios_base::app
-                                     );
-  sink->set_formatter(formatter);
-  boost::log::core::get()->add_sink(sink);
+  boost::log::add_file_log(boost::log::keywords::auto_flush = true,
+                           boost::log::keywords::format = "[%TimeStamp%] <%Severity%> : %Message%",
+                           boost::log::keywords::file_name = m_filename + ".log",
+                           boost::log:: keywords::open_mode = std::ios_base::app
+                          );
   boost::log::add_common_attributes();
 
 }
 
 te::core::Logger::Logger()
 {
-  SetupLogger(m_filename, m_path);
+  boost::log::register_simple_formatter_factory< boost::log::trivial::severity_level, char >("Severity");
+  setupLogger(m_filename);
 }
 
 void te::core::Logger::log(const std::string& message, boost::log::trivial::severity_level severity)
