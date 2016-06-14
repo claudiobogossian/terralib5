975f3af9f92c472db6cde8bd1cfb556e27cda92b
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2016-06-14 14:54:13 -0300

    code review: Logger unittest

--- unittest/core/logger/TsLogger.cpp
+++ unittest/core/logger/TsLogger.cpp
@@ -40,7 +40,6 @@ BOOST_AUTO_TEST_SUITE(logger_test_case)
 BOOST_AUTO_TEST_CASE(default_logger_test)
 {
   BOOST_CHECK_NO_THROW(TE_INIT_LOGGER("logs/mylogs.log"));
-  BOOST_CHECK(boost::filesystem::file_size(te::core::FindInTerraLibPath("logs/mylogs.log")) > 0);
   BOOST_CHECK_NO_THROW(TE_LOGGER_FORMAT("[%TimeStamp%]{%ThreadID%} %Process%(%ProcessID%) <%Severity%>: %Message%"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_WARN("Warning log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_INFO("Info log"));
@@ -48,6 +47,7 @@ BOOST_AUTO_TEST_CASE(default_logger_test)
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_TRACE("Trace log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_FATAL("Fatal log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_DEBUG("Debug log"));
+  BOOST_CHECK(boost::filesystem::file_size(te::core::FindInTerraLibPath("logs/mylogs.log")) > 0);
 }
 
 BOOST_AUTO_TEST_CASE(configuration_file_logger_test)
@@ -55,13 +55,13 @@ BOOST_AUTO_TEST_CASE(configuration_file_logger_test)
   BOOST_CHECK_THROW(TE_INIT_LOGGER_FROM_FILE("mylogger.ini"), std::exception);
   BOOST_CHECK_NO_THROW(TE_INIT_LOGGER_FROM_FILE(te::core::FindInTerraLibPath("share/terralib/config/te-log-unittest.ini")));
   BOOST_CHECK_THROW(TE_LOGGER_FORMAT("[%TimeStamp%]{%ThreadID%} %Process%(%ProcessID%) <%Severity%>: %Message%"), te::Exception);
-  BOOST_CHECK(boost::filesystem::file_size(te::core::FindInTerraLibPath("logs/terralib_unittest_core.log")) > 0);
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_WARN("Warning log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_INFO("Info log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_ERROR("Error log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_TRACE("Trace log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_FATAL("Fatal log"));
   BOOST_CHECK_NO_THROW(TE_LOG_CORE_DEBUG("Debug log"));
+  BOOST_CHECK(boost::filesystem::file_size(te::core::FindInTerraLibPath("logs/terralib_unittest_core.log")) > 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
