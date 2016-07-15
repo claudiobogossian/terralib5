4d7367f4ed4a4bfcec0939c608b3447a5395123d
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2016-07-15 09:17:00 -0300

    Changed how the files are written to disk

--- src/terralib/xerces/Writer.cpp
+++ src/terralib/xerces/Writer.cpp
@@ -31,6 +31,9 @@
 #include "XMLChToStr.h"
 #include "Writer.h"
 
+// STL
+#include <fstream>
+
 // Boost
 #include <boost/lexical_cast.hpp>
 
@@ -85,14 +88,13 @@ void te::xerces::Writer::writeToFile()
 #endif
 
   xercesc::XMLFormatTarget* myFormTarget = 0;
-
   try
   {
     // Se nao tem filename, eh stdout
     if( m_uri.empty() )
       myFormTarget = new xercesc::StdOutFormatTarget();
     else
-      myFormTarget = new xercesc::LocalFileFormatTarget( m_uri.c_str() );
+      myFormTarget = new xercesc::MemBufFormatTarget();
 
 #if XERCES_VERSION_MAJOR == 2
     theSerializer->writeNode( myFormTarget, *doc_ );
@@ -100,8 +102,11 @@ void te::xerces::Writer::writeToFile()
     theOutput = ( (xercesc::DOMImplementationLS*)m_impl)->createLSOutput();
     theOutput->setByteStream(myFormTarget);
     theSerializer->write(m_doc, theOutput );
+    std::string xmlstring = (char*)((xercesc::MemBufFormatTarget*)myFormTarget)->getRawBuffer();
+    std::ofstream out(m_uri.c_str());
+    out << xmlstring;
+    out.close();
 #endif
-
     delete theOutput;
     delete theSerializer;
     delete myFormTarget;
