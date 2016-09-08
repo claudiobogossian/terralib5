cdaa61f3a2517fe3f6e7beb5d84ebfa8973b2d74
felipe.siqueira <felipe.siqueira@funcate.org.br>
2016-09-08 08:57:20 -0300

    close #1566

--- src/terralib/terralib4/VectorDataSet.cpp
+++ src/terralib/terralib4/VectorDataSet.cpp
@@ -298,6 +298,8 @@ std::string terralib4::VectorDataSet::getString(std::size_t i) const
 
   m_instance.getPropertyValue(val, ii);
 
+  val = te::core::CharEncoding::convert(val, te::core::EncodingType::LATIN1, te::core::EncodingType::UTF8);
+
   return val;
 }
 
