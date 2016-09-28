aa4f1df82e0d9ce76b4bbbdf5f13c5579bdadd20
Matheus C. Zaglia <mzaglia@dpi.inpe.br>
2016-09-28 14:12:17 -0300

    core/plugin improvement

--- examples/core/plugin/te_example_core_plugin2/Plugin2.cpp
+++ examples/core/plugin/te_example_core_plugin2/Plugin2.cpp
@@ -35,14 +35,17 @@ TERRALIB_CPP_PLUGIN_BEGIN(Plugin2)
 
 TERRALIB_CPP_PLUGIN_STARTUP
 {
+  if(m_initialized)
+    return;
   std::cout << "Plugin2 startup" << std::endl;
+  m_initialized = true;
 }
 
 TERRALIB_CPP_PLUGIN_SHUTDOWN
 {
   std::cout << "Plugin2 shutdown" << std::endl;
+  m_initialized = false;
 }
-
 TERRALIB_CPP_PLUGIN_END
 
 TERRALIB_PLUGIN_CALL_BACK_IMPL(Plugin2)
